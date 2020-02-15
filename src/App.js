fda1d682440789323501bfe8a0b7754c346a0d43
Vitalii <vitaliybondarenko1982@gmail.com>
2020-02-15 23:21:22 +0200

    on review

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 import React, { Component } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
+import { MovieSearch } from './components/MovieSearch/MovieSearch';
 import moviesFromServer from './api/movies.json';
 
+const getFilteredMovies = (movies, query) => {
+  const queryToLowerCase = query.toLowerCase();
+  const filteredMovies = movies.filter((movie) => {
+    const titleToLowerCase = movie.title.toLowerCase();
+    const descriptionToLowerCase = movie.description.toLowerCase();
+
+    return (
+      titleToLowerCase.includes(queryToLowerCase)
+      || descriptionToLowerCase.includes(queryToLowerCase)
+    );
+  });
+
+  return filteredMovies;
+};
+
 export class App extends Component {
-  state = {};
+  state = {
+    query: '',
+  };
+
+  queryChangeHandler = (query) => {
+    this.setState({ query });
+  };
 
   render() {
+    const { query } = this.state;
+
     return (
       <div className="page">
         <div className="page-content">
-          <div className="box">
-            <div className="field">
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
-
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
-            </div>
-          </div>
-
-          <MoviesList movies={moviesFromServer} />
+          <MovieSearch
+            query={this.state.query}
+            changeHandler={this.queryChangeHandler}
+          />
+          <MoviesList
+            movies={getFilteredMovies(moviesFromServer, query)}
+          />
         </div>
         <div className="sidebar">
           Sidebar goes here
