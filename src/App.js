4c7dfef9017b7baecc8037862e2f4c08f617c8c9
Oleg <fasfas7971@gmail.com>
2020-04-27 16:05:08 +0300

    My solution

--- src/App.js
+++ src/App.js
@@ -2,32 +2,34 @@ import React, { Component } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import Input from './components/Search';
 
 export class App extends Component {
-  state = {};
+  state = {
+    query: moviesFromServer,
+  };
+
+  search = (event) => {
+    const searchPhrase = event.target.value.toLowerCase();
+    const newMovies = moviesFromServer.filter((film) => {
+      const title = film.title.toLowerCase();
+      const description = film.description.toLowerCase();
+
+      return title.includes(searchPhrase)
+      || description.includes(searchPhrase);
+    });
+
+    this.setState(() => ({
+      query: newMovies,
+    }));
+  }
 
   render() {
     return (
       <div className="page">
         <div className="page-content">
-          <div className="box">
-            <div className="field">
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
-
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
-            </div>
-          </div>
-
-          <MoviesList movies={moviesFromServer} />
+          <Input param={this.search} />
+          <MoviesList movies={this.state.query} />
         </div>
         <div className="sidebar">
           Sidebar goes here
