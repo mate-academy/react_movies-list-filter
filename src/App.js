95a48a9363727bc90e271040ed30b076fcae49cc
Kostya <oliinyk.kostya@gmail.com>
2020-04-30 23:27:57 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export class App extends Component {
-  state = {};
+  state = {
+    text: '',
+  };
+
+  handleChange = (event) => {
+    this.setState({ text: event.target.value });
+  }
 
   render() {
+    const { text } = this.state;
+    const gi = 'gi';
+    const regesp = new RegExp(text, gi);
+
     return (
       <div className="page">
         <div className="page-content">
@@ -19,15 +29,20 @@ export class App extends Component {
               <div className="control">
                 <input
                   type="text"
+                  value={this.state.text}
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  onChange={this.handleChange}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList
+            movies={
+              [...moviesFromServer].filter(item => item.title.match(regesp))}
+          />
         </div>
         <div className="sidebar">
           Sidebar goes here
