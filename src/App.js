4e4e4f73c0787d4c975a981a9692b1f7d992b550
Anatoliy Pokhylko <pokhilko.av@gmail.com>
2020-09-23 18:26:10 +0300

    Implement 4 point

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
+import { MovieFilter } from './components/MovieFilter';
 import moviesFromServer from './api/movies.json';
 
 export class App extends Component {
@@ -22,29 +23,16 @@ export class App extends Component {
   }
 
   render() {
+    const { query, movies } = this.state;
+
     return (
       <div className="page">
         <div className="page-content">
-          <div className="box">
-            <div className="field">
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
-
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                  value={this.state.query}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </div>
-          </div>
-
-          <MoviesList movies={this.state.movies} />
+          <MovieFilter
+            query={query}
+            handleChange={this.handleChange}
+          />
+          <MoviesList movies={movies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
