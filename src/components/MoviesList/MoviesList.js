2f305d7aad4076fb045c8c96b2ba32693f18b900
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-10-03 07:28:49 +0300

    Solution

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -4,13 +4,21 @@ import PropTypes from 'prop-types';
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} {...movie} />
-    ))}
-  </div>
-);
+export const MoviesList = ({ movies }) => {
+  if (movies.length === 0) {
+    return (
+      <h1>This movie was not found</h1>
+    );
+  }
+
+  return (
+    <div className="movies">
+      {movies.map(movie => (
+        <MovieCard key={movie.imdbId} {...movie} />
+      ))}
+    </div>
+  );
+};
 
 MoviesList.propTypes = {
   movies: PropTypes.arrayOf(
