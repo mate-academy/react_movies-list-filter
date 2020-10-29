94f81977fe21e0cc97181afed61d34cf6a74c61a
Anton <office.sklyarov@gmail.com>
2020-10-29 15:35:12 +0200

    added movies list filter

--- src/App.js
+++ src/App.js
@@ -4,10 +4,28 @@ import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export class App extends Component {
-  state = {};
+  state = {
+    query: '',
+  };
+
+  saveInput = (event) => {
+    const { value } = event.target;
+
+    this.setState({
+      query: value.toLowerCase(),
+    });
+  }
 
   render() {
+    const { query } = this.state;
+
+    const visibleMovies = moviesFromServer.filter(({ title, description }) => (
+      title.toLowerCase().includes(query)
+      || description.toLowerCase().includes(query)
+    ));
+
     return (
+
       <div className="page">
         <div className="page-content">
           <div className="box">
@@ -22,12 +40,13 @@ export class App extends Component {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  onChange={this.saveInput}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
