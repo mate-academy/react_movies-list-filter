bfe67c4a8b371a2d806942878cfbc35cf4e4b8e0
Yuriy Putsan <putsan57@gmail.com>
2020-10-29 18:32:22 +0200

    fix search

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -4,13 +4,22 @@ import PropTypes from 'prop-types';
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} {...movie} />
-    ))}
-  </div>
-);
+export const MoviesList = ({ movies, query }) => {
+  const filteredMovies = movies.filter((movie) => {
+    const { title, description } = movie;
+    const cardText = `${title} ${description}`;
+
+    return cardText.toLowerCase().includes(query);
+  });
+
+  return (
+    <div className="movies">
+      {filteredMovies.map(movie => (
+        <MovieCard key={movie.imdbId} {...movie} />
+      ))}
+    </div>
+  );
+};
 
 MoviesList.propTypes = {
   movies: PropTypes.arrayOf(
@@ -21,8 +30,10 @@ MoviesList.propTypes = {
       imdbUrl: PropTypes.string.isRequired,
     }),
   ),
+  query: PropTypes.string,
 };
 
 MoviesList.defaultProps = {
   movies: [],
+  query: '',
 };
