8babfdcab965429bbd8de8a678c095e1173c9a9d
Eugene <evolokhin@gmail.com>
2020-10-29 19:17:57 +0200

    solution

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -4,15 +4,23 @@ import PropTypes from 'prop-types';
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
+export const MoviesList = ({ movies, query }) => (
   <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} {...movie} />
-    ))}
+    {movies.map((movie) => {
+      const titleNormalized = movie.title.toLowerCase();
+      const descriptionNormalized = movie.description.toLowerCase();
+      const visible = titleNormalized.includes(query)
+        || descriptionNormalized.includes(query);
+
+      return (
+        visible && <MovieCard key={movie.imdbId} {...movie} />
+      );
+    })}
   </div>
 );
 
 MoviesList.propTypes = {
+  query: PropTypes.string.isRequired,
   movies: PropTypes.arrayOf(
     PropTypes.shape({
       title: PropTypes.string.isRequired,
