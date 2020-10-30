0b6b0cdf102bc383e0a9330999c45d345cf68624
goncharukop <goncharukop@gmail.com>
2020-10-30 12:15:28 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
 import React, { Component } from 'react';
 import './App.scss';
+import { Search } from './components/Search';
 import { MoviesList } from './components/MoviesList';
+
 import moviesFromServer from './api/movies.json';
 
 export class App extends Component {
-  state = {};
+  state = {
+    query: '',
+    moviesList: moviesFromServer,
+  };
+
+  moviesFilter = (query) => {
+    const filterWord = query.toLowerCase();
+
+    this.setState({
+      query,
+      moviesList: moviesFromServer.filter(movie => (
+        movie.title.toLowerCase().includes(filterWord)
+        || movie.description.toLowerCase().includes(filterWord)
+      )),
+    });
+  }
 
   render() {
+    const { query, moviesList } = this.state;
+
     return (
       <div className="page">
         <div className="page-content">
           <div className="box">
             <div className="field">
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
-
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
+              <Search
+                query={query}
+                moviesFilter={this.moviesFilter}
+              />
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={moviesList} />
         </div>
         <div className="sidebar">
           Sidebar goes here
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Search = ({ query, moviesFilter }) => (
+  <div className="field">
+    <label htmlFor="search-query" className="label">
+      Search movie
+    </label>
+    <div className="control">
+      <input
+        type="text"
+        id="search-query"
+        value={query}
+        className="input"
+        placeholder="Type search word"
+        onChange={event => (
+          moviesFilter(event.target.value)
+        )}
+      />
+    </div>
+  </div>
+);
+
+Search.propTypes = {
+  query: PropTypes.string.isRequired,
+  moviesFilter: PropTypes.func.isRequired,
+};
