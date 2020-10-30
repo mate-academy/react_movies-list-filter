87440de0ddf6219ca6fb75db60f843750b82048b
Sofiia Kucherenko <kucherenko.sv13@gmail.com>
2020-10-31 00:11:30 +0300

    fixed component and method

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,34 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export class SearchBar extends React.Component {
-  handleChange = (event) => {
-    this.props.filterByQuery(event);
-  }
-
-  render() {
-    const { enteredText } = this.props;
-
-    return (
-      <div className="field">
-        <label htmlFor="enteredText" className="label">
-          Search movie
-        </label>
-        <div className="control">
-          <input
-            type="text"
-            id="enteredText"
-            className="input"
-            placeholder="Type search word"
-            name="enteredText"
-            value={enteredText}
-            onChange={this.handleChange}
-          />
-        </div>
-      </div>
-    );
-  }
-}
+export const SearchBar = ({ enteredText, filterByQuery }) => (
+  <div className="field">
+    <label htmlFor="enteredText" className="label">
+      Search movie
+    </label>
+    <div className="control">
+      <input
+        type="text"
+        id="enteredText"
+        className="input"
+        placeholder="Type search word"
+        name="enteredText"
+        value={enteredText}
+        onChange={filterByQuery}
+      />
+    </div>
+  </div>
+);
 
 SearchBar.propTypes = {
   filterByQuery: PropTypes.func.isRequired,
