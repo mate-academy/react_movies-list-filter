4d02bbfea59d042d2df255d91bfb1843963987e9
AntonChetveryk <chetverykanton92@gmail.com>
2020-11-09 11:38:47 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
+/* eslint-disable operator-linebreak */
+/* eslint-disable comma-dangle */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable arrow-parens */
 import React, { Component } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import { Search } from './components/Search/Search';
 
 export class App extends Component {
-  state = {};
+  state = {
+    query: '',
+  };
+
+  onChange = ({ target }) => {
+    this.setState({ query: target.value });
+  };
 
   render() {
+    const { query } = this.state;
+    const movies = [...moviesFromServer].filter((movie) => {
+      const title = movie.title.toLowerCase();
+      const description = movie.description.toLowerCase();
+
+      return (
+        title.includes(query.toLowerCase()) ||
+        description.includes(query.toLowerCase())
+      );
+    });
+
     return (
       <div className="page">
         <div className="page-content">
-          <div className="box">
-            <div className="field">
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
-
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
-            </div>
-          </div>
-
-          <MoviesList movies={moviesFromServer} />
+          <Search query={query} onChange={this.onChange} />
+          <MoviesList movies={movies} />
         </div>
-        <div className="sidebar">
-          Sidebar goes here
-        </div>
+        <div className="sidebar">Sidebar goes here</div>
       </div>
     );
   }
