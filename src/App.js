dacc9d532a395b221cd6c7fee37e4a6e5dcb1463
Layrovell <marina95prodan@gmail.com>
2021-01-21 22:23:16 +0200

    fixed after a code review

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@ import React from 'react';
 import './App.scss';
 import moviesFromServer from './api/movies.json';
 import { SearchField } from './components/SearchField/SearchField';
+import { MoviesList } from './components/MoviesList/MoviesList';
 
-export const App = () => (
-  <div className="page">
-    <SearchField movies={moviesFromServer} />
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    movies: moviesFromServer,
+    query: '',
+  };
+
+  searchMovie = () => {
+    const searchQuery = this.state.query.toLocaleLowerCase().trim();
+    const filteredMovies = this.state.movies
+      .filter(movie => movie.title.toLocaleLowerCase().includes(searchQuery)
+        || movie.description.toLocaleLowerCase().includes(searchQuery));
+
+    return filteredMovies;
+  }
+
+  target = (e) => {
+    this.setState({ query: e });
+  }
+
+  render() {
+    return (
+      <div className="page">
+        <div className="page-content">
+          <div className="box">
+            <SearchField query={this.state.query} target={this.target} />
+          </div>
+          <MoviesList movies={this.searchMovie()} />
+        </div>
+        <div className="sidebar">
+          Sidebar goes here
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -1,55 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { MoviesList } from '../MoviesList';
 
-export class SearchField extends React.Component {
-  state = {
-    query: '',
-  };
-
-  render() {
-    const { movies } = this.props;
-
-    const searchQuery = this.state.query.toLocaleLowerCase().trim();
-    const filteredMovies = [...movies]
-      .filter(movie => movie.title.toLocaleLowerCase().includes(searchQuery)
-        || movie.description.toLocaleLowerCase().includes(searchQuery));
-
-    return (
-      <div className="page-content">
-
-        <div className="box">
-          <div className="field">
-            <label htmlFor="search-query" className="label">
-              Search movie
-            </label>
-            <div className="control">
-              <input
-                onChange={(e) => {
-                  this.setState({
-                    query: e.target.value,
-                  });
-                }}
-                value={this.state.query}
-                type="text"
-                id="search-query"
-                className="input"
-                placeholder="Type search word"
-              />
-            </div>
-          </div>
-        </div>
-        <MoviesList movies={filteredMovies} />
-      </div>
-    );
-  }
-}
+export const SearchField = ({ query, target }) => (
+  <div className="field">
+    <label htmlFor="search-query" className="label">
+      Search movie
+    </label>
+    <div className="control">
+      <input
+        onChange={(e) => {
+          target(e.target.value);
+        }}
+        value={query}
+        type="text"
+        id="search-query"
+        className="input"
+        placeholder="Type search word"
+      />
+    </div>
+  </div>
+);
 
 SearchField.propTypes = {
-  movies: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
+  target: PropTypes.func.isRequired,
+  query: PropTypes.string.isRequired,
 };
