52d28287981577e7ee20b403c5886007c29ef9b3
Alexandr Petrenko <alexandr.petrenko.fe@gmail.com>
2021-01-25 01:04:14 +0200

    solution

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -4,13 +4,21 @@ import PropTypes from 'prop-types';
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} {...movie} />
-    ))}
-  </div>
-);
+export const MoviesList = ({ movies, query }) => {
+  const lowerQuery = query.toLowerCase();
+  const visibleMovie = movies.filter(movie => (
+    movie.title.toLowerCase().includes(lowerQuery)
+    || movie.description.toLowerCase().includes(lowerQuery)
+  ));
+
+  return (
+    <div className="movies">
+      {visibleMovie.map(movie => (
+        <MovieCard key={movie.imdbId} {...movie} />
+      ))}
+    </div>
+  );
+};
 
 MoviesList.propTypes = {
   movies: PropTypes.arrayOf(
@@ -21,8 +29,10 @@ MoviesList.propTypes = {
       imdbUrl: PropTypes.string.isRequired,
     }),
   ),
+  query: PropTypes.string,
 };
 
 MoviesList.defaultProps = {
   movies: [],
+  query: '',
 };
