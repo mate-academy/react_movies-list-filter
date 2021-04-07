9b519f678c25f724fc014ab9adf0b2883a526db1
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-04-07 14:44:49 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -2,32 +2,31 @@ import React, { Component } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import { SearchQuery } from './components/SearchQuery';
 
 export class App extends Component {
-  state = {};
+  state = {
+    visibleFilms: [...moviesFromServer],
+  };
+
+  filterFilm = (value) => {
+    const lowerQuery = value.toLowerCase().trim();
+
+    this.setState({
+      visibleFilms: moviesFromServer
+        .filter(film => film.title.toLowerCase().includes(lowerQuery)
+              || film.description.toLowerCase().includes(lowerQuery)),
+    });
+  }
 
   render() {
     return (
       <div className="page">
         <div className="page-content">
           <div className="box">
-            <div className="field">
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
-
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
-            </div>
+            <SearchQuery filterFilm={this.filterFilm} />
           </div>
-
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={this.state.visibleFilms} />
         </div>
         <div className="sidebar">
           Sidebar goes here
