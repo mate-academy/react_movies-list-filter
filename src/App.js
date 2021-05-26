b3f382c8eefa051365c47577bc0b7014c1e30586
amithos <alex.theutos@gmail.com>
2021-05-26 17:40:35 +0300

    movies list filter

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export class App extends Component {
-  state = {};
+  state = {
+    query: '',
+  };
+
+  safeQuery = (event) => {
+    this.setState({
+      query: event.target.value,
+    });
+  }
 
   render() {
+    const { query } = this.state;
+    const lowerQuery = query.toLowerCase();
+
+    // eslint-disable-next-line max-len
+    const visibleMovies = moviesFromServer.filter(movie => movie.title.toLowerCase().includes(lowerQuery)
+      || movie.description.toLowerCase().includes(lowerQuery));
+
     return (
       <div className="page">
         <div className="page-content">
@@ -22,12 +37,14 @@ export class App extends Component {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  value={this.state.query}
+                  onChange={this.safeQuery}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
