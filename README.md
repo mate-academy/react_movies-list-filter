19bd6c2c1990cf28a1576ea735c5f6692c2b8299
Leonid Vegera <leonid.vegera@gmail.com>
2021-05-26 23:22:55 +0300

    Movies filter

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Movies list - Filter
 - Replace `<your_account>` with your Github username in the
- [DEMO LINK](https://<your_account>.github.io/react_movies-list-filter/)
+ [DEMO LINK](https://leonid-vegera.github.io/react_movies-list-filter/)
 - Follow the [React task guideline](https://github.com/mate-academy/react_task-guideline#react-tasks-guideline)
 
 ## Task
--- src/App.js
+++ src/App.js
@@ -4,9 +4,23 @@ import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export class App extends Component {
-  state = {};
+  state = {
+    query: '',
+  };
+
+  handleQueryChange = (event) => {
+    this.setState({
+      query: event.target.value,
+    });
+  };
 
   render() {
+    const { query } = this.state;
+    const normalizedQuery = query.toLowerCase();
+    const visibleFilm = moviesFromServer.filter(film => (
+      film.title.toLocaleLowerCase().includes(normalizedQuery)
+      || film.description.toLocaleLowerCase().includes(normalizedQuery)));
+
     return (
       <div className="page">
         <div className="page-content">
@@ -22,12 +36,14 @@ export class App extends Component {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  value={this.state.query}
+                  onChange={this.handleQueryChange}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleFilm} />
         </div>
         <div className="sidebar">
           Sidebar goes here
