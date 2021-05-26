b5a22823c8ad43ed8963b42157843d6ed443386f
Ruslana Podshyvalova <lanataylorpersonal@gmail.com>
2021-05-26 15:37:43 +0300

    solution

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { MoviesList } from '../MoviesList';
+
+export class Search extends React.Component {
+  state = {
+    query: '',
+  }
+
+  render() {
+    const { moviesList } = this.props;
+    const { query } = this.state;
+    let filteredMoveisList = [];
+
+    if (query) {
+      const searchPhrase = query.toLocaleLowerCase();
+
+      filteredMoveisList = moviesList.filter(({ title, description }) => (
+        title.toLocaleLowerCase().includes(searchPhrase)
+            || description.toLocaleLowerCase().includes(searchPhrase)
+      ));
+    }
+
+    return (
+      <>
+        <div className="box">
+          <div className="field">
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={this.state.query}
+                onChange={(event) => {
+                  this.setState({ query: event.target.value });
+                }}
+              />
+            </div>
+          </div>
+        </div>
+
+        <MoviesList movies={query ? filteredMoveisList : moviesList} />
+      </>
+    );
+  }
+}
+
+Search.propTypes = {
+  moviesList: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string,
+    }),
+  ),
+};
+
+Search.defaultProps = {
+  moviesList: [],
+};
