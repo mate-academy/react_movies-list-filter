9f70007c510af0840502d1e83cd223a61de4fcbb
Ivan Grekov <grekovv76@gmail.com>
2021-05-26 19:23:42 +0300

    component name of FilteredList  and names of arguments of function filterByTwoParams were changed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { PureComponent } from 'react';
 import './App.scss';
 import moviesFromServer from './api/movies.json';
-import { FilteredList } from './components/FilteredList';
+import { SearchForm } from './components/SearchForm';
 
 export class App extends PureComponent {
   render() {
     return (
       <div className="page">
         <div className="page-content">
-          <FilteredList list={moviesFromServer} />
+          <SearchForm list={moviesFromServer} />
         </div>
         <div className="sidebar">
           Sidebar goes here
