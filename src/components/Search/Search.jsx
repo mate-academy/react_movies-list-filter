44056404a2275a5b9fd8a3879275bf468466ca49
Pavel Kuzmenko <pashokkuzmenko@gmail.com>
2021-05-26 10:42:06 +0300

    Solution v0.1

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { MoviesList } from '../MoviesList';
+
+export class Search extends Component {
+  state = {
+    query: '',
+  };
+
+  render() {
+    const { movies } = this.props;
+    const textToLower = this.state.query.toLocaleLowerCase();
+    const visibleMovies = [...movies].filter(({ title, description }) => (
+      title.toLocaleLowerCase().includes(textToLower)
+        || description.toLocaleLowerCase().includes(textToLower)
+    ));
+
+    return (
+      <>
+        <div className="box">
+          <div className="field">
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={this.state.query}
+                onChange={event => this.setState({
+                  query: event.target.value,
+                })}
+              />
+            </div>
+          </div>
+        </div>
+        <MoviesList movies={visibleMovies} />
+      </>
+    );
+  }
+}
+
+Search.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
