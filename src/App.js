7e6f04fbb6b5da4c78994ea4e433669c34420785
Minchuk Olga <olga@MacBook-Pro-Olga.local>
2021-05-27 04:17:26 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -4,35 +4,54 @@ import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export class App extends Component {
-  state = {};
+  state = {
+    query: '',
+    findMovies: moviesFromServer,
+  };
 
-  render() {
-    return (
-      <div className="page">
-        <div className="page-content">
-          <div className="box">
-            <div className="field">
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
+changeQuery = (event) => {
+  this.setState({
+    query: event.target.value,
+    findMovies: moviesFromServer.filter(
+      movie => movie.title
+        .toLowerCase()
+        .includes(event.target.value.toLowerCase())
+        || movie.description
+          .toLowerCase()
+          .includes(event.target.value.toLowerCase()),
+    ),
+  });
+}
 
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
+render() {
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={this.state.query}
+                onChange={this.changeQuery}
+              />
             </div>
           </div>
+        </div>
 
-          <MoviesList movies={moviesFromServer} />
-        </div>
-        <div className="sidebar">
-          Sidebar goes here
-        </div>
+        <MoviesList movies={this.state.findMovies} />
       </div>
-    );
-  }
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
+    </div>
+  );
+}
 }
