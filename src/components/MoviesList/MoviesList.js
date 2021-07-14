951b1e9c7260bf758bc57bdd4fe11bef65f3a258
Tolik <tolikdolinskiy@gmail.com>
2021-07-14 13:24:11 +0300

    some message

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -4,13 +4,42 @@ import PropTypes from 'prop-types';
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} {...movie} />
-    ))}
-  </div>
-);
+export const MoviesList = ({ movies, valueSearch }) => {
+  const copyMovies = [...movies];
+
+  copyMovies.sort((prevMovie, nextMovie) => {
+    const prevSearchResTitle = (
+      prevMovie.title.toLowerCase().indexOf(valueSearch));
+    const prevSearchResDescript = (
+      prevMovie.description.toLowerCase().indexOf(valueSearch));
+    const nextSearchResTitle = (
+      nextMovie.title.toLowerCase().indexOf(valueSearch));
+    const nextSearchResDescript = (
+      nextMovie.description.toLowerCase().indexOf(valueSearch));
+
+    if (prevSearchResTitle !== nextSearchResTitle
+      || prevSearchResDescript !== nextSearchResDescript) {
+      if (prevSearchResTitle === 0
+        || (prevSearchResDescript === 0 && nextSearchResTitle !== 0)) {
+        return -1;
+      }
+
+      if (nextSearchResTitle === 0 || nextSearchResDescript === 0) {
+        return 1;
+      }
+    }
+
+    return 0;
+  });
+
+  return (
+    <div className="movies">
+      {copyMovies.map(movie => (
+        <MovieCard key={movie.imdbId} {...movie} />
+      ))}
+    </div>
+  );
+};
 
 MoviesList.propTypes = {
   movies: PropTypes.arrayOf(
@@ -21,6 +50,7 @@ MoviesList.propTypes = {
       imdbUrl: PropTypes.string.isRequired,
     }),
   ),
+  valueSearch: PropTypes.string.isRequired,
 };
 
 MoviesList.defaultProps = {
