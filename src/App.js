9b493dd6da130486c4fcff5cb5107dadd1c9739e
Viktor Dzhyranov <v.dzhyranov@gmail.com>
2021-07-20 16:49:36 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ import SearchBar from './components/SearchBar/SearchBar';
 export class App extends Component {
   state = {
     query: '',
+    movies: [...moviesFromServer],
+  };
+
+  getFindedMovies = () => {
+    const queryToLowerCase = this.state.query.toLocaleLowerCase();
+    const filtred = this.state.movies.filter(movie => (
+      movie.title.toLowerCase().indexOf(queryToLowerCase) !== -1
+        || movie.description.toLowerCase().indexOf(queryToLowerCase) !== -1
+    ));
+
+    return filtred;
   };
 
   setAppState = (value) => {
@@ -22,7 +33,7 @@ export class App extends Component {
           </div>
 
           <MoviesList
-            movies={moviesFromServer}
+            movies={this.getFindedMovies()}
             query={this.state.query}
           />
         </div>
--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -4,16 +4,11 @@ import PropTypes from 'prop-types';
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies, query }) => (
+export const MoviesList = ({ movies }) => (
   <div className="movies">
-    {movies
-      .filter(movie => (
-        movie.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
-        || movie.description.toLowerCase().indexOf(query.toLowerCase()) !== -1
-      ))
-      .map(movie => (
-        <MovieCard key={movie.imdbId} {...movie} />
-      ))}
+    {movies.map(movie => (
+      <MovieCard key={movie.imdbId} {...movie} />
+    ))}
   </div>
 );
 
@@ -26,7 +21,6 @@ MoviesList.propTypes = {
       imdbUrl: PropTypes.string.isRequired,
     }),
   ),
-  query: PropTypes.string.isRequired,
 };
 
 MoviesList.defaultProps = {
