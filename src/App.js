904c45d9f6a7c17d4ebb3a59db73a62ce7fe9547
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-07-26 17:48:26 +0300

    feat: completed task filter-list

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import React, { Component } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
+import { SearchField } from './components/SearchField/SearchField';
 import moviesFromServer from './api/movies.json';
 
 export class App extends Component {
-  state = {};
+  state = {
+    query: '',
+  };
+
+  addQuery = (event) => {
+    this.setState({
+      query: event.target.value,
+    });
+  }
 
   render() {
+    const { query } = this.state;
+    const queryToLower = query.toLowerCase();
+
+    const choiceMovie = moviesFromServer.filter(
+      movie => movie.description.toLowerCase().includes(
+        queryToLower,
+      )
+    || movie.title.toLowerCase().includes(queryToLower),
+    );
+
     return (
       <div className="page">
         <div className="page-content">
-          <div className="box">
-            <div className="field">
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
+          <SearchField value={query} onChange={this.addQuery} />
 
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
-            </div>
-          </div>
-
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={choiceMovie} />
         </div>
         <div className="sidebar">
           Sidebar goes here
