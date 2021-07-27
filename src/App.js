61d664057b7be2c14c6b0796c02366c09ce1f8a6
Tetiana Yasko <taniayasko2015@gmail.com>
2021-07-27 15:36:19 +0300

    changed state

--- src/App.js
+++ src/App.js
@@ -7,25 +7,29 @@ import './App.scss';
 export class App extends Component {
   state = {
     query: '',
+    visibleMovies: moviesFromServer,
   };
 
   handleQueryChange = (event) => {
-    this.setState({ query: event.target.value });
+    this.setState({
+      query: event.target.value,
+      visibleMovies: this.filteredMovies(moviesFromServer),
+    });
   }
 
   filteredMovies = movies => movies.filter(
     movie => movie.title.toLowerCase().includes(this.state.query.toLowerCase())
     || movie.description.toLowerCase().includes(this.state.query.toLowerCase()),
-  );
+  )
 
   render() {
-    const visibleMovies = this.filteredMovies(moviesFromServer);
+    const { query, visibleMovies } = this.state;
 
     return (
       <div className="page">
         <div className="page-content">
           <SearchMovie
-            value={this.state.query}
+            value={query}
             onChange={this.handleQueryChange}
           />
           <MoviesList movies={visibleMovies} />
