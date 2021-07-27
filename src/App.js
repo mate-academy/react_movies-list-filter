83c3325fae0106c34508feea9e5f9884736fd95a
Aleksandr Semerenko <sanya96088475@gmail.com>
2021-07-27 17:19:29 +0300

    react movie list

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export class App extends Component {
-  state = {};
+  state = {
+    query: '',
+  };
+
+  handleChange = (event) => {
+    const { value } = event.target;
+
+    this.setState({
+      query: value.toLowerCase(),
+    });
+  }
 
   render() {
+    const { query } = this.state;
+    const filteredFilms = moviesFromServer.filter(movie => movie
+      .description.toLowerCase().includes(query)
+      || movie.title.toLowerCase().includes(query));
+
     return (
       <div className="page">
         <div className="page-content">
@@ -22,12 +37,14 @@ export class App extends Component {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  value={query}
+                  onChange={this.handleChange}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={filteredFilms} />
         </div>
         <div className="sidebar">
           Sidebar goes here
