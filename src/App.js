1ac3c8f78510dc2e5afc7e41f444b1877b0925e9
NYaremchuk <nazarmails@gmail.com>
2021-07-29 15:10:30 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export class App extends Component {
-  state = {};
+  state = {
+    query: '',
+  };
 
   render() {
+    const { query } = this.state;
+
     return (
       <div className="page">
         <div className="page-content">
@@ -22,12 +26,20 @@ export class App extends Component {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  onChange={(event) => {
+                    this.setState({
+                      query: event.target.value,
+                    });
+                  }}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList
+            movies={moviesFromServer}
+            query={query}
+          />
         </div>
         <div className="sidebar">
           Sidebar goes here
