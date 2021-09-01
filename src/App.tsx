d9122337ded2754412780cbe55b1277fd795e317
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-09-01 12:34:48 +0300

    Resolved

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,24 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string,
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
 
   render() {
+    const filteredMovies = moviesFromServer.filter(cards => {
+      const modQuery = this.state.query.toLowerCase().trim();
+      const tittle = cards.title.toLowerCase();
+      const description = cards.description.toLowerCase();
+
+      return tittle.includes(modQuery) || description.includes(modQuery);
+    });
+
     return (
       <div className="page">
         <div className="page-content">
@@ -24,12 +36,18 @@ export class App extends React.Component<{}, State> {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  value={this.state.query}
+                  onChange={(event) => {
+                    this.setState({
+                      query: event.target.value,
+                    });
+                  }}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={filteredMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
