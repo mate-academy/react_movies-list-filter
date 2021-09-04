918e018d0026e7020875fcf03d9617eebb2ab6f7
Illia Vovchenko <ilya.vovchenko@gmail.com>
2021-09-04 15:57:04 +0300

    task done

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,34 @@ import React from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import { SearchBox } from './components/SearchBox';
 
-type State = {};
+type State = {
+  movies: Movie[];
+  visibleMovies: Movie[];
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    movies: moviesFromServer,
+    visibleMovies: moviesFromServer,
+  };
+
+  handleMoviesChange = (newMovies: Movie[]) => {
+    this.setState({ visibleMovies: newMovies });
+  };
 
   render() {
+    const { movies, visibleMovies } = this.state;
+
     return (
       <div className="page">
         <div className="page-content">
-          <div className="box">
-            <div className="field">
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
-
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
-            </div>
-          </div>
-
-          <MoviesList movies={moviesFromServer} />
+          <SearchBox
+            movies={movies}
+            handleMoviesChange={this.handleMoviesChange}
+          />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
