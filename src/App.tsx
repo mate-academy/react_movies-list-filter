9e9972facea5815447e47d82cbe9fd4752ca9928
Moroz Dmytro <dmytro101100@gmail.com>
2021-09-08 20:01:38 +0300

    The task has been done

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
 import React from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
+import { SearchBar } from './components/SearchBar';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string;
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      query: event.target.value,
+    });
+  };
+
+  filterMovies = () => {
+    const query = this.state.query.toLowerCase();
+
+    return moviesFromServer.filter(
+      movie => {
+        const { title, description } = movie;
+
+        return title.toLowerCase().includes(query)
+          || description.toLowerCase().includes(query);
+      },
+    );
+  };
 
   render() {
+    const visibleMovies = this.filterMovies();
+
     return (
       <div className="page">
         <div className="page-content">
-          <div className="box">
-            <div className="field">
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
-
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
-            </div>
-          </div>
-
-          <MoviesList movies={moviesFromServer} />
+          <SearchBar query={this.state.query} queryChange={this.handleChange} />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
