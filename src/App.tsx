40377113eb666c8da6cd3cf423061bb0cb300e8a
dmytro_mykoliv <amigo.del.ua@gmail.com>
2021-09-09 10:23:25 +0200

    react movie list filter

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string,
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
 
   render() {
+    const searchMovie = moviesFromServer
+      .filter(({ title, description }) => (
+        title.toLowerCase().includes(this.state.query.toLowerCase())
+        || description.toLowerCase().includes(this.state.query.toLowerCase())
+      ));
+
     return (
       <div className="page">
         <div className="page-content">
@@ -24,9 +34,20 @@ export class App extends React.Component<{}, State> {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  value={this.state.query}
+                  onChange={(event) => {
+                    this.setState({ query: event.target.value });
+                  }}
                 />
               </div>
             </div>
+          </div>
+
+          <div className="show-search">
+            <MoviesList movies={searchMovie} />
+            <hr />
+            <hr />
+            <hr />
           </div>
 
           <MoviesList movies={moviesFromServer} />
