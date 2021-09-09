34ba71fff440cc6564674f74ecdb7e40337a1b47
Dima Tkachenko <DimaTkachenko3@gmail.com>
2021-09-09 19:11:37 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,33 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string;
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state = {
+    query: '',
+  };
+
+  searchChange = (event: React.ChangeEvent<HTMLInputElement>) => (
+    this.setState({ query: event.target.value })
+  );
+
+  getFilteredMove = () => {
+    const { query } = this.state;
+
+    const filteredMove = [...moviesFromServer].filter((movie) => (
+      (movie.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+    || movie.description.toLocaleLowerCase().includes(query.toLocaleLowerCase()))
+    ));
+
+    return filteredMove;
+  };
 
   render() {
+    const filteredMove = this.getFilteredMove();
+
     return (
       <div className="page">
         <div className="page-content">
@@ -24,12 +45,13 @@ export class App extends React.Component<{}, State> {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  value={this.state.query}
+                  onChange={this.searchChange}
                 />
               </div>
             </div>
           </div>
-
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={filteredMove} />
         </div>
         <div className="sidebar">
           Sidebar goes here
