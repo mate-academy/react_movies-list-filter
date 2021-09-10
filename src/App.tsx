bba2f9cf447650d24859b1bab56dc7a94cce577c
Sasha Havryliuk <akwaccc@gmail.com>
2021-09-10 14:59:10 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,31 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  moviesList: Movie[],
+  query: string;
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    moviesList: moviesFromServer,
+    query: '',
+  };
+
+  filteredList = (movie: Movie) => {
+    const lowerTitle = movie.title.toLowerCase();
+    const lowerDescription = movie.description.toLowerCase();
+    const lowerQuery = this.state.query.toLowerCase();
+
+    return (
+      (lowerTitle.includes(lowerQuery) || lowerDescription.includes(lowerQuery))
+    );
+  };
 
   render() {
+    const visibleList = [...this.state.moviesList]
+      .filter(movie => this.filteredList(movie));
+
     return (
       <div className="page">
         <div className="page-content">
@@ -24,12 +43,16 @@ export class App extends React.Component<{}, State> {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  value={this.state.query}
+                  onChange={(event) => {
+                    this.setState({ query: event.target.value });
+                  }}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleList} />
         </div>
         <div className="sidebar">
           Sidebar goes here
