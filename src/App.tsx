888c8b9e4b7a561cf74fa38a85018e64ff5e6192
Andrii Hryhoruk <andriihryhoruk5544@gmail.com>
2021-09-10 18:53:58 +0300

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
+/* eslint-disable react/no-unused-state */
 import React from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string;
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    this.setState({ query: value });
+  };
 
   render() {
+    const { query } = this.state;
+
     return (
       <div className="page">
         <div className="page-content">
@@ -22,14 +35,20 @@ export class App extends React.Component<{}, State> {
                 <input
                   type="text"
                   id="search-query"
+                  name="query"
                   className="input"
                   placeholder="Type search word"
+                  value={query}
+                  onChange={this.handleChange}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList
+            movies={moviesFromServer}
+            query={query}
+          />
         </div>
         <div className="sidebar">
           Sidebar goes here
