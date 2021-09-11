8fcec84a035a463a4a7d733815997884f369ccde
kristof22122 <kristf44@gmail.com>
2021-09-11 11:03:17 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,44 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string;
+  movies: Movie[];
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+    movies: [...moviesFromServer],
+  };
+
+  search = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      query: event.target.value,
+    });
+  };
+
+  getVisibleMovies = () => {
+    const { query, movies } = this.state;
+
+    let visibleMovies = movies;
+
+    if (query) {
+      const lowerQuery = query.toLocaleLowerCase();
+
+      visibleMovies = movies
+        .filter(movie => movie.title.toLocaleLowerCase().includes(lowerQuery)
+        || movie.description.toLocaleLowerCase().includes(lowerQuery));
+    }
+
+    return visibleMovies;
+  };
 
   render() {
+    const { query } = this.state;
+
+    const visibleMovies = this.getVisibleMovies();
+
     return (
       <div className="page">
         <div className="page-content">
@@ -24,12 +56,16 @@ export class App extends React.Component<{}, State> {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  value={query}
+                  onChange={this.search}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          {visibleMovies.length !== 0 && (
+            <MoviesList movies={visibleMovies} />
+          )}
         </div>
         <div className="sidebar">
           Sidebar goes here
