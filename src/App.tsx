fe4375ae5bb3a0c5ac549c58979387484f737add
zadorojniiarsen <zadorojniiarsen@gmail.com>
2021-09-12 19:42:56 +0300

    first try

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string;
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state = {
+    query: '',
+  };
 
   render() {
+    const { query } = this.state;
+
     return (
       <div className="page">
         <div className="page-content">
@@ -24,12 +30,22 @@ export class App extends React.Component<{}, State> {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  value={query}
+                  onChange={(e) => {
+                    this.setState({ query: e.target.value });
+                  }}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={
+            moviesFromServer.filter(
+              movie => movie.title.toLowerCase().includes(this.state.query.toLowerCase())
+              || movie.description.toLowerCase().includes(this.state.query.toLowerCase()),
+            )
+          }
+          />
         </div>
         <div className="sidebar">
           Sidebar goes here
