45a47b6710e7320b79670356019bd829a639469b
Vova-Stoliar <stoliarmoney@gmail.com>
2021-09-12 14:47:20 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
+/* eslint-disable no-console */
 import React from 'react';
+import { Movie } from './type/Movie';
 import './App.scss';
+import { searchMovie } from './components/serchMovie/serchMovie';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string,
+  movieStorage: Movie[],
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  moviesFromServerClone = moviesFromServer.map(movie => {
+    return {
+      ...movie,
+    };
+  });
+
+  state: State = {
+    query: '',
+    movieStorage: this.moviesFromServerClone,
+  };
 
   render() {
+    const { movieStorage, query } = this.state;
+
     return (
       <div className="page">
         <div className="page-content">
@@ -24,12 +41,14 @@ export class App extends React.Component<{}, State> {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  value={this.state.query}
+                  onChange={(event) => this.setState({ query: event.target.value })}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={searchMovie(movieStorage, query)} />
         </div>
         <div className="sidebar">
           Sidebar goes here
