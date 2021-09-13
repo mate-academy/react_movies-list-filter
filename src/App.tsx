6417dda16f7496dc4192fd68eed5ec13a5008218
Oleksandr Makohon <sashamakogon94@gmail.com>
2021-09-13 07:41:31 +0300

    done task list filter

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,45 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string;
+  movies: Movie[];
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+    movies: [...moviesFromServer],
+  };
+
+  handleChange = (event: any) => {
+    const { value } = event.target;
+
+    this.setState({ query: value.toLowerCase() });
+  };
+
+  searchFilm = () => {
+    const { query } = this.state;
+
+    this.setState({
+      movies: [...moviesFromServer].filter(movie => (
+        movie.title.toLowerCase().includes(query)
+        || movie.description.toLowerCase().includes(query)
+      )),
+    });
+  };
 
   render() {
     return (
       <div className="page">
         <div className="page-content">
-          <div className="box">
+          <form
+            method="post"
+            className="box"
+            onSubmit={(event) => {
+              event.preventDefault();
+            }}
+          >
             <div className="field">
               <label htmlFor="search-query" className="label">
                 Search movie
@@ -24,12 +53,23 @@ export class App extends React.Component<{}, State> {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  value={this.state.query}
+                  onChange={this.handleChange}
                 />
               </div>
-            </div>
-          </div>
 
-          <MoviesList movies={moviesFromServer} />
+              <button
+                type="submit"
+                className="control-button"
+                onClick={this.searchFilm}
+              >
+                Search
+              </button>
+
+            </div>
+          </form>
+
+          <MoviesList movies={this.state.movies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
