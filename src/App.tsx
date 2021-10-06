2dbb17c8bc483d6f84eb43866b1f0200cfeca42f
Artem Fedoseev <afedoseev359@gmail.com>
2021-10-06 13:30:50 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,39 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string,
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
+
+  setQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ query: event.target.value.toLowerCase() });
+  };
+
+  filterMovies = () => {
+    const { query } = this.state;
+
+    return moviesFromServer.filter(movie => {
+      const { title, description } = movie;
+
+      return title.toLowerCase().includes(query)
+             || description.toLowerCase().includes(query);
+    });
+  };
 
   render() {
+    const filteredMovies = this.filterMovies();
+
     return (
       <div className="page">
         <div className="page-content">
           <div className="box">
             <div className="field">
+              {/* eslint-disable-next-line */}
               <label htmlFor="search-query" className="label">
                 Search movie
               </label>
@@ -21,6 +43,8 @@ export class App extends React.Component<{}, State> {
               <div className="control">
                 <input
                   type="text"
+                  value={this.state.query}
+                  onChange={this.setQuery}
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
@@ -29,7 +53,7 @@ export class App extends React.Component<{}, State> {
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={filteredMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
