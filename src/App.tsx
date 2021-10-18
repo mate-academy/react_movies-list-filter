e57cca8f05d2a9fcd6ab9c464d7b83a881a9cdaf
Kostiantyn Besedin <besconstantine@gmail.com>
2021-10-18 18:49:32 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,37 @@ import React from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import { Search } from './components/Search';
 
-type State = {};
+type State = {
+  query: string,
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
+
+  changedQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    this.setState({
+      query: value,
+    });
+  };
 
   render() {
+    const { query } = this.state;
+
     return (
       <div className="page">
         <div className="page-content">
           <div className="box">
             <div className="field">
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
-
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
+              <Search query={query} callback={(event) => this.changedQuery(event)} />
             </div>
           </div>
-
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={moviesFromServer} query={query.toLocaleLowerCase()} />
         </div>
         <div className="sidebar">
           Sidebar goes here
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -4,12 +4,22 @@ import { MovieCard } from '../MovieCard';
 
 interface Props {
   movies: Movie[];
+  query: string;
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
+export const MoviesList: React.FC<Props> = ({ movies, query }) => (
   <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
+    {movies
+      .filter(item => {
+        let { title, description } = item;
+
+        title = title.toLocaleLowerCase();
+        description = description.toLocaleLowerCase();
+
+        return !query || (title.includes(query) || description.includes(query));
+      })
+      .map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
   </div>
 );
