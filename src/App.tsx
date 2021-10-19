e4d8ad45b50a9e17f64ae5619bdb6f9ffa97bf18
Igor Igor <you@example.com>
2021-10-19 11:47:19 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string,
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
 
   render() {
     return (
@@ -16,20 +20,24 @@ export class App extends React.Component<{}, State> {
             <div className="field">
               <label htmlFor="search-query" className="label">
                 Search movie
+                <div className="control">
+                  <input
+                    type="text"
+                    id="search-query"
+                    className="input"
+                    placeholder="Type search word"
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                      this.setState(({
+                        query: event.currentTarget.value,
+                      }));
+                    }}
+                  />
+                </div>
               </label>
-
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={moviesFromServer} query={this.state.query} />
         </div>
         <div className="sidebar">
           Sidebar goes here
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -4,11 +4,19 @@ import { MovieCard } from '../MovieCard';
 
 interface Props {
   movies: Movie[];
+  query: string
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
+export const MoviesList: React.FC<Props> = ({ movies, query }) => (
   <div className="movies">
-    {movies.map(movie => (
+    {movies.filter(movie => {
+      const seachQuery = query.toLowerCase();
+      const movieTitle = movie.title.toLowerCase();
+      const movieDescription = movie.description.toLowerCase();
+
+      return movieTitle.includes(seachQuery)
+      || movieDescription.includes(seachQuery);
+    }).map(movie => (
       <MovieCard key={movie.imdbId} movie={movie} />
     ))}
   </div>
