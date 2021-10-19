5aa6827d8a6533ddccb1cf6d319a21e772814a5a
Oksana Onopa <onopa.kseni@gmail.com>
2021-10-19 13:33:16 +0300

    error fixed

--- src/App.tsx
+++ src/App.tsx
@@ -31,18 +31,18 @@ export class App extends React.Component<{}, State> {
             <div className="field">
               <label htmlFor="search-query" className="label">
                 Search movie
-              </label>
 
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                  value={query}
-                  onChange={this.handlerChange}
-                />
-              </div>
+                <div className="control">
+                  <input
+                    type="text"
+                    id="search-query"
+                    className="input"
+                    placeholder="Type search word"
+                    value={query}
+                    onChange={this.handlerChange}
+                  />
+                </div>
+              </label>
             </div>
           </div>
 
