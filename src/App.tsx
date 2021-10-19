5efd55c7b64bdcaf155b15573acc1b5d69445964
AVronskyi <vronskyi.a@gmail.com>
2021-10-19 15:19:27 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,30 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state = {
+    query: '',
+  };
+
+  queryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ query: event.target.value });
+  };
+
+  searchMovie = () => {
+    const { query } = this.state;
+    const queryToLowerCase = query.toLocaleLowerCase();
+
+    const movies = moviesFromServer.filter(movie => (
+      movie.title.toLocaleLowerCase().includes(queryToLowerCase)
+      || movie.description.toLowerCase().includes(queryToLowerCase)
+    ));
+
+    return movies;
+  };
 
   render() {
     return (
@@ -16,20 +36,21 @@ export class App extends React.Component<{}, State> {
             <div className="field">
               <label htmlFor="search-query" className="label">
                 Search movie
+                <div className="control">
+                  <input
+                    type="text"
+                    id="search-query"
+                    className="input"
+                    placeholder="Type search word"
+                    value={this.state.query}
+                    onChange={this.queryChange}
+                  />
+                </div>
               </label>
-
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={this.searchMovie()} />
         </div>
         <div className="sidebar">
           Sidebar goes here
