5da6fcc948a847737b99479019614d88876fb2fc
PresidentComanch <ap.dissembler@gmail.com>
2021-10-19 22:47:02 +0300

    --done main task

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,64 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+interface Movie {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl:string,
+  imdbId: string,
+}
+
+type State = {
+  movies: Movie[],
+  query: string;
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    movies: moviesFromServer,
+    query: '',
+  };
+
+  textInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const query = event.currentTarget.value;
+
+    this.setState(state => ({
+      ...state,
+      query,
+    }));
+  };
 
   render() {
+    const moviesVisible = this.state.movies.filter(movie => (
+      movie.title.toLowerCase().includes(this.state.query.toLowerCase())
+      || movie.description.toLowerCase().includes(this.state.query.toLowerCase())
+    ));
+
     return (
       <div className="page">
         <div className="page-content">
           <div className="box">
             <div className="field">
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
-
               <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
+                <label
+                  htmlFor="search-query"
+                  className="label"
+                >
+                  Search movie
+                  <input
+                    type="text"
+                    id="search-query"
+                    className="input"
+                    placeholder="Type search word"
+                    onChange={this.textInputHandler}
+                  />
+                </label>
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={moviesVisible} />
         </div>
         <div className="sidebar">
           Sidebar goes here
