ca772bc7585405a6dc62aa92fe637c802ec1f4ed
Maksym Vikolash <vikolashmaxim@gmail.com>
2021-10-20 12:10:25 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import { Component } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string,
+};
 
-export class App extends React.Component<{}, State> {
-  state: State = {};
+export class App extends Component<{}, State> {
+  state: State = {
+    query: '',
+  };
+
+  onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ query: event.target.value });
+  };
 
   render() {
+    const { query } = this.state;
+
+    const filteredMovies = moviesFromServer.filter(
+      movie => movie.title.toLowerCase()
+        .includes(query.toLowerCase()),
+    );
+
     return (
       <div className="page">
         <div className="page-content">
@@ -16,20 +31,21 @@ export class App extends React.Component<{}, State> {
             <div className="field">
               <label htmlFor="search-query" className="label">
                 Search movie
+                <div className="control">
+                  <input
+                    type="text"
+                    id="search-query"
+                    className="input"
+                    placeholder="Type search word"
+                    value={query}
+                    onChange={this.onChangeHandler}
+                  />
+                </div>
               </label>
-
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={filteredMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
