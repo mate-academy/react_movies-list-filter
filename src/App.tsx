b13e07594ac6c93829a07b370579d84df4aa60b0
Oleksandr Saulko <aleksandrsaulko@gmail.com>
2021-11-06 12:03:17 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string;
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
+
+  formHandler = (event: { target: { value: any; }; }) => {
+    const { value } = event.target;
+
+    this.setState({
+      query: value,
+    });
+  };
 
   render() {
+    const { query } = this.state;
+    const visibleMovies = moviesFromServer.filter(movie => (
+      movie.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+      || movie.description.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+    ));
+
     return (
       <div className="page">
         <div className="page-content">
           <div className="box">
             <div className="field">
-              <label htmlFor="search-query" className="label">
+              <label
+                htmlFor="search-query"
+                className="label"
+              >
                 Search movie
               </label>
 
@@ -24,12 +46,14 @@ export class App extends React.Component<{}, State> {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  value={this.state.query}
+                  onChange={this.formHandler}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
