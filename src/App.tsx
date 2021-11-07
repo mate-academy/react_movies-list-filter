e4a54bd4a9d6fd1484e3d552854434fe452f01a3
MaxStets <weawer458@gmail.com>
2021-11-07 22:19:40 +0200

    Solution -- react_movies-list-filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string;
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
+
+  handleSearch = (value: string) => {
+    this.setState({ query: value });
+  };
+
+  filterMovies = () => {
+    const queryFilter = this.state.query.toLowerCase();
+
+    return moviesFromServer.filter((movie) => {
+      const { title, description } = movie;
+
+      return title.toLocaleLowerCase().includes(queryFilter)
+        || description.toLocaleLowerCase().includes(queryFilter);
+    });
+  };
 
   render() {
+    const { query } = this.state;
+    const visibleMovies = this.filterMovies();
+
     return (
       <div className="page">
         <div className="page-content">
@@ -24,12 +47,14 @@ export class App extends React.Component<{}, State> {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  value={query}
+                  onChange={(event) => this.handleSearch(event.target.value)}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
