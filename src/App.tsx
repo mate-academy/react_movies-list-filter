61801a969350a371b077abd1c5850b4841dd4229
Mariia Obraztsova <mariiaobraztsova99@gmail.com>
2021-11-11 12:10:51 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,21 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
 
   render() {
+    const visibleMovies = moviesFromServer.filter(
+      movie => movie.title.toLowerCase().includes(this.state.query)
+      || movie.description.toLowerCase().includes(this.state.query),
+    );
+
     return (
       <div className="page">
         <div className="page-content">
@@ -16,20 +25,26 @@ export class App extends React.Component<{}, State> {
             <div className="field">
               <label htmlFor="search-query" className="label">
                 Search movie
-              </label>
 
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
+                <div className="control">
+                  <input
+                    type="text"
+                    id="{search-query}"
+                    className="input"
+                    placeholder="Type search word"
+                    value={this.state.query}
+                    onChange={(event) => {
+                      this.setState({
+                        query: event.target.value.toLowerCase(),
+                      });
+                    }}
+                  />
+                </div>
+              </label>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
