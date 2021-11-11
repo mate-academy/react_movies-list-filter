e445767603aa154f71d44119a09af7da03e3ee0c
cyb1ove <cyb1ove.ua@gmail.com>
2021-11-11 20:45:18 +0300

    Task was solved

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,32 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+interface State {
+  query: string;
+}
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
+
+  updateState = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      query: event.target.value,
+    });
+  };
+
+  hasSimilarInQuery = (...args: string[]) => {
+    return args.some(value => value.toLowerCase().includes(this.state.query));
+  };
 
   render() {
+    const visibleMovies = moviesFromServer.filter(movie => {
+      const { title, description } = movie;
+
+      return this.hasSimilarInQuery(title, description);
+    });
+
     return (
       <div className="page">
         <div className="page-content">
@@ -24,12 +44,13 @@ export class App extends React.Component<{}, State> {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  onChange={(event) => this.updateState(event)}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
