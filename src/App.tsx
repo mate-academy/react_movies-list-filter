38baa25f0c64fcb50f72bd7d7bdfae8a8c4428e1
Olga Bordiug <obordiug@gmail.com>
2021-11-11 20:54:22 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  movies: Movie[],
+  query: string,
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    movies: moviesFromServer,
+    query: '',
+  };
+
+  handelChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ query: event.target.value });
+  };
 
   render() {
+    const { movies, query } = this.state;
+
+    const visibleMovies = (query === '')
+      ? movies
+      : movies.filter(
+        movie => movie.title.toLowerCase().includes(query.toLowerCase())
+        || movie.description.toLowerCase().includes(query.toLowerCase()),
+      );
+
     return (
       <div className="page">
         <div className="page-content">
@@ -24,12 +44,14 @@ export class App extends React.Component<{}, State> {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  value={query}
+                  onChange={this.handelChange}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
