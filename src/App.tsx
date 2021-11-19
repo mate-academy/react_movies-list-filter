fcf9a4ed9068754dfbee271a603735441be73b96
Aleksey Tsybulko <tsybulko.aleksey@gmail.com>
2021-11-19 14:45:34 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,25 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string,
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
 
   render() {
+    const { query } = this.state;
+    const visibleMivies = moviesFromServer.filter(movie => {
+      const value = query.toLocaleLowerCase();
+      const title = movie.title.toLocaleLowerCase();
+      const description = movie.description.toLocaleLowerCase();
+
+      return title.includes(value) || description.includes(value);
+    });
+
     return (
       <div className="page">
         <div className="page-content">
@@ -16,20 +29,24 @@ export class App extends React.Component<{}, State> {
             <div className="field">
               <label htmlFor="search-query" className="label">
                 Search movie
+                <div className="control">
+                  <input
+                    type="text"
+                    name="search"
+                    id="search-query"
+                    className="input"
+                    placeholder="Type search word"
+                    value={this.state.query}
+                    onChange={(event) => {
+                      this.setState({ query: event.target.value });
+                    }}
+                  />
+                </div>
               </label>
-
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMivies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
