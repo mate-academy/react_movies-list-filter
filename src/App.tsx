32e93f13fc9f1636f47a6de2cbea9994b73dc3af
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2021-11-26 15:02:50 +0200

    Task Done

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,27 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string;
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
+
+  handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ query: event.target.value.toUpperCase() });
+  };
 
   render() {
+    const { query } = this.state;
+
+    const visibleMovies = moviesFromServer.filter(movie => {
+      return movie.title.toUpperCase().includes(query)
+        || movie.description.toUpperCase().includes(query);
+    });
+
     return (
       <div className="page">
         <div className="page-content">
@@ -24,12 +39,16 @@ export class App extends React.Component<{}, State> {
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  onChange={this.handleSearch}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={query === ''
+            ? moviesFromServer
+            : visibleMovies}
+          />
         </div>
         <div className="sidebar">
           Sidebar goes here
