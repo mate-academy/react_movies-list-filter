782778e3f56700b3b04ec0920621612953e46660
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2021-11-26 18:44:13 +0200

    Implement search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,56 @@
 import React from 'react';
 import './App.scss';
+
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import './types/Movie';
 
-type State = {};
+type State = {
+  query: string;
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      query: event.target.value,
+    });
+  };
 
   render() {
+    const { query } = this.state;
+    const queryRegExp = new RegExp(query, 'i');
+
+    const visibleMovies: Movie[] = moviesFromServer.filter(
+      (movie) => queryRegExp.test(movie.title) || queryRegExp.test(movie.description),
+    );
+
     return (
       <div className="page">
         <div className="page-content">
           <div className="box">
             <div className="field">
-              <label htmlFor="search-query" className="label">
+              <label htmlFor="search-query">
                 Search movie
+                <div className="control">
+                  <input
+                    type="text"
+                    id="search-query"
+                    className="input"
+                    placeholder="Type search word"
+                    onChange={this.handleChange}
+                  />
+                </div>
               </label>
-
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovies} />
         </div>
-        <div className="sidebar">
-          Sidebar goes here
-        </div>
+        <div className="sidebar">Sidebar goes here</div>
       </div>
     );
   }
