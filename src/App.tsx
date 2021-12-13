8e680cbf417a545f4a8a4738254fafdc0c12382a
Dariia Romanova <dariia.romanova@gmail.com>
2021-12-13 19:07:42 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,25 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string,
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
 
   render() {
+    const { query } = this.state;
+    const visibleMovies = moviesFromServer.filter(movie => {
+      const title = movie.title.toLocaleLowerCase();
+      const description = movie.description.toLocaleLowerCase();
+      const serchQury = query.toLocaleLowerCase();
+
+      return (title.includes(serchQury) || description.includes(serchQury));
+    });
+
     return (
       <div className="page">
         <div className="page-content">
@@ -16,20 +29,24 @@ export class App extends React.Component<{}, State> {
             <div className="field">
               <label htmlFor="search-query" className="label">
                 Search movie
-              </label>
 
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
+                <div className="control">
+                  <input
+                    type="text"
+                    id="search-query"
+                    className="input"
+                    placeholder="Type search word"
+                    value={this.state.query}
+                    onChange={(event) => {
+                      this.setState({ query: event.target.value });
+                    }}
+                  />
+                </div>
+              </label>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
