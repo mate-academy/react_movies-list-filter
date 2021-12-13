1f83fdd2831862f5e5394e51ccf3104617005f9a
Maryna Saikun <marynasaikun@gmail.com>
2021-12-13 14:58:40 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,39 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+interface Movie {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+}
+
+type State = {
+  moviesList: Movie[]
+  searchParam: string,
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    moviesList: moviesFromServer,
+    searchParam: '',
+  };
 
   render() {
+    const { moviesList, searchParam } = this.state;
+
+    let visibleMovies = moviesList;
+
+    if (searchParam.length > 0) {
+      visibleMovies = moviesList.filter(movie => {
+        const { title, description } = movie;
+
+        return title.toLowerCase().includes(searchParam.toLowerCase())
+          || description.toLowerCase().includes(searchParam.toLowerCase());
+      });
+    }
+
     return (
       <div className="page">
         <div className="page-content">
@@ -16,20 +43,21 @@ export class App extends React.Component<{}, State> {
             <div className="field">
               <label htmlFor="search-query" className="label">
                 Search movie
-              </label>
-
-              <div className="control">
                 <input
                   type="text"
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  value={this.state.searchParam}
+                  onChange={(event) => {
+                    this.setState({ searchParam: event.target.value });
+                  }}
                 />
-              </div>
+              </label>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
