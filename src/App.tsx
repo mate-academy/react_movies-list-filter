911a90fa992a4fa53af4749efbb98808591118b4
Lera Litfullina <litfullina@icloud.com>
2021-12-13 17:38:38 +0300

    movies filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string;
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
 
   render() {
+    const visibleMovies = moviesFromServer.filter(movie => {
+      const queryInLower = this.state.query.toLowerCase();
+      const titleInLower = movie.title.toLowerCase();
+      const decriptionInLower = movie.description.toLowerCase();
+
+      return (
+        titleInLower.includes(queryInLower) || decriptionInLower.includes(queryInLower)
+      );
+    });
+
     return (
       <div className="page">
         <div className="page-content">
@@ -23,13 +38,16 @@ export class App extends React.Component<{}, State> {
                   type="text"
                   id="search-query"
                   className="input"
+                  onChange={(event) => {
+                    this.setState({ query: event.target.value });
+                  }}
                   placeholder="Type search word"
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
