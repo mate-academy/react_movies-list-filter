d9c36db9843f6dbeb57547eedb9ec3ec15b0f074
Vladyslav Gonsales <www.magicbox@gmail.com>
2021-12-15 00:33:48 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,54 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string,
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      query: event.target.value.toLowerCase(),
+    });
+  };
+
+  filterMovie = (movies: Movie[]) => (
+    movies.filter((movie) => (
+      (movie.title.toLowerCase().includes(this.state.query.toLowerCase()))
+      || (movie.description.toLowerCase().includes(this.state.query.toLowerCase()))
+    ))
+  );
 
   render() {
+    const { query } = this.state;
+    const visibleMovies = this.filterMovie(moviesFromServer);
+
     return (
       <div className="page">
         <div className="page-content">
           <div className="box">
             <div className="field">
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
-
               <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
+                <label htmlFor="search-query" className="label">
+                  Search movie
+                  <input
+                    type="text"
+                    id="search-query"
+                    className="input"
+                    placeholder="Type search word"
+                    value={query}
+                    onChange={this.handleChange}
+                  />
+                </label>
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
