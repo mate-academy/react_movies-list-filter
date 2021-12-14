e87b3de201d321ea74ebafcf8fd601741672e35e
Oleksiy Pochkayev <oleksiypochkayev@gmail.com>
2021-12-14 22:38:49 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,34 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  searchQuery: string;
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    searchQuery: '',
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      searchQuery: event.target.value,
+    });
+  };
 
   render() {
+    const visibleMovies = [...moviesFromServer].filter((film) => {
+      const isTitleIcnludeSearchQuery = (
+        film.title.toLowerCase().includes(this.state.searchQuery.toLowerCase())
+      );
+
+      const isDescriptionIncludeSearchQuery = (
+        film.description.toLowerCase().includes(this.state.searchQuery.toLowerCase())
+      );
+
+      return isTitleIcnludeSearchQuery || isDescriptionIncludeSearchQuery;
+    });
+
     return (
       <div className="page">
         <div className="page-content">
@@ -21,15 +43,18 @@ export class App extends React.Component<{}, State> {
               <div className="control">
                 <input
                   type="text"
+                  name="searchQuery"
                   id="search-query"
+                  value={this.state.searchQuery}
                   className="input"
                   placeholder="Type search word"
+                  onChange={this.handleChange}
                 />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
