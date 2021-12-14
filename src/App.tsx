49cbcaadef49b755638ed0f34256e7d18d800afb
Ivan Meleganych <imi92@ukr.net>
2021-12-14 14:06:53 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,49 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string;
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
 
   render() {
+    const visibleMovie = moviesFromServer.filter(movie => {
+      const queryLower = this.state.query.toLowerCase();
+      const titleLower = movie.title.toLowerCase();
+      const descriptionLower = movie.description.toLowerCase();
+
+      return (
+        titleLower.includes(queryLower) || descriptionLower.includes(queryLower)
+      );
+    });
+
     return (
       <div className="page">
         <div className="page-content">
           <div className="box">
             <div className="field">
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
-
               <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
+                <label htmlFor="search-query" className="label">
+                  Search movie
+                  <input
+                    type="text"
+                    id="search-query"
+                    className="input"
+                    onChange={(event) => {
+                      this.setState({ query: event.target.value });
+                    }}
+                    placeholder="Type search word"
+                  />
+                </label>
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovie} />
         </div>
         <div className="sidebar">
           Sidebar goes here
