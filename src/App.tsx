66e82b62e74366afb0a5ac9b73df33650e90e636
Vitalii Lytvyn <vitalii.lytvyn01@gmail.com>
2021-12-14 21:59:44 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,38 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type Movie = {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+};
+
+type State = {
+  query: string,
+  filteredList: Movie[],
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+    filteredList: moviesFromServer,
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ query: event.target.value.toLowerCase() });
+    this.searchByQuery();
+  };
+
+  searchByQuery = () => {
+    this.setState((state) => ({
+      filteredList: moviesFromServer.filter((movie) => {
+        return movie.title.toLowerCase().includes(state.query)
+        || movie.description.toLowerCase().includes(state.query);
+      }),
+    }));
+  };
 
   render() {
     return (
@@ -16,20 +44,22 @@ export class App extends React.Component<{}, State> {
             <div className="field">
               <label htmlFor="search-query" className="label">
                 Search movie
+                <div className="control">
+                  <input
+                    type="text"
+                    id="search-query"
+                    className="input"
+                    placeholder="Type search word"
+                    // value={this.state.query}
+                    onChange={this.handleChange}
+                  />
+                </div>
               </label>
 
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={this.state.filteredList} />
         </div>
         <div className="sidebar">
           Sidebar goes here
