156de868762e9a264f0a375c1913ccc76358d3d7
Ivan Stanko <ivan_stanko@ukr.net>
2021-12-15 07:58:43 +0200

    completed react movies list filter

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,34 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string,
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { target } = event;
+
+    this.setState({
+      query: target.value,
+    });
+  };
+
+  checkMovies = (...args: string[]) => {
+    return args.some(movie => movie.toLowerCase().includes(this.state.query.toLowerCase()));
+  };
 
   render() {
+    const { query } = this.state;
+
+    const visibleMovies = moviesFromServer.filter(
+      movie => this.checkMovies(movie.title, movie.description),
+    );
+
     return (
       <div className="page">
         <div className="page-content">
@@ -16,20 +38,19 @@ export class App extends React.Component<{}, State> {
             <div className="field">
               <label htmlFor="search-query" className="label">
                 Search movie
-              </label>
-
-              <div className="control">
                 <input
                   type="text"
                   id="search-query"
                   className="input"
                   placeholder="Type search word"
+                  value={query}
+                  onChange={this.handleChange}
                 />
-              </div>
+              </label>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
