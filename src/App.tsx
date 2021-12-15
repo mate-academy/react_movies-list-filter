397bb8c96148d21409a367f5f9ac8de89578ec2d
Valeriia Dlugash <lera.dlugash@gmail.com>
2021-12-15 15:08:07 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,50 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  query: string;
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    query: '',
+  };
+
+  handleOnChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+    const oneLetter = event.target.value;
+    const callback = (prevState: State) => {
+      const { query } = prevState;
+      const newQuery = query + oneLetter;
+      const result = {
+        query: newQuery,
+      };
+
+      return result;
+    };
+
+    this.setState(callback);
+  };
+
+  filterMovies = () => {
+    return (
+      moviesFromServer.filter((movie) => {
+        const queryLowerCase = this.state.query.toLowerCase();
+        const movieTitleLowerCase = movie.title.toLowerCase();
+        const movieDescriptionLowerCase = movie.description.toLowerCase();
+
+        return (
+          movieDescriptionLowerCase.includes(queryLowerCase)
+          || movieTitleLowerCase.includes(queryLowerCase)
+        );
+      })
+    );
+  };
 
   render() {
+    const { query } = this.state;
+    const { handleOnChange, filterMovies } = this;
+    const visibleMovies = filterMovies();
+
     return (
       <div className="page">
         <div className="page-content">
@@ -16,20 +54,22 @@ export class App extends React.Component<{}, State> {
             <div className="field">
               <label htmlFor="search-query" className="label">
                 Search movie
+                <div className="control">
+                  <input
+                    type="text"
+                    id="search-query"
+                    className="input"
+                    placeholder="Type search word"
+                    onChange={handleOnChange}
+                    value={query}
+                  />
+                </div>
               </label>
 
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
