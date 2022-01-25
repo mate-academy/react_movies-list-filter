b0ef0919877fb709d8d306527568d5f24ef6e1d1
Bohdan Maksymiuk <bohdan.max@ukr.net>
2022-01-25 10:31:45 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  movies: Movie[],
+  query: string,
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    movies: moviesFromServer,
+    query: '',
+  };
+
+  handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      query: event.currentTarget.value,
+    });
+  };
+
+  moviesToShow = () => {
+    const moviesCopy = [...this.state.movies];
+    const query = this.state.query.toLowerCase();
+
+    return moviesCopy.filter(movie => (
+      movie.title.toLowerCase().includes(query)
+        || movie.description.toLowerCase().includes(query)
+    ));
+  };
 
   render() {
+    const visibleMovies = this.moviesToShow();
+
     return (
       <div className="page">
         <div className="page-content">
@@ -20,6 +45,8 @@ export class App extends React.Component<{}, State> {
 
               <div className="control">
                 <input
+                  value={this.state.query}
+                  onChange={this.handleQueryChange}
                   type="text"
                   id="search-query"
                   className="input"
@@ -29,7 +56,7 @@ export class App extends React.Component<{}, State> {
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
