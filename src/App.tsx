1ccc64423299f1e68f06b4d2ea45629d99f4f5a8
Mikedacheka <mikedacheka@gmail.com>
2022-01-26 12:23:16 +0200

    sm msg

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,23 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {};
+type State = {
+  searchWord: string,
+};
 
 export class App extends React.Component<{}, State> {
-  state: State = {};
+  state: State = {
+    searchWord: '',
+  };
+
+  filteredMovies = () => {
+    const { searchWord } = this.state;
+
+    return moviesFromServer.filter(
+      movie => movie.title.toLowerCase().includes(searchWord.toLocaleLowerCase())
+      || movie.description.toLowerCase().includes(searchWord.toLocaleLowerCase()),
+    );
+  };
 
   render() {
     return (
@@ -16,20 +29,24 @@ export class App extends React.Component<{}, State> {
             <div className="field">
               <label htmlFor="search-query" className="label">
                 Search movie
+                <div className="control">
+                  <input
+                    type="text"
+                    id="search-query"
+                    onChange={(event) => {
+                      this.setState({
+                        searchWord: event?.target.value,
+                      });
+                    }}
+                    className="input"
+                    placeholder="Type search word"
+                  />
+                </div>
               </label>
-
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
-              </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={this.filteredMovies()} />
         </div>
         <div className="sidebar">
           Sidebar goes here
