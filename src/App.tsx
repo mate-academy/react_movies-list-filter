e2d8dc0b257bf78a0de755e299f72291ccf5b03e
Олег Майборода <xa12850446@student.karazin.ua>
2022-02-21 19:06:29 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+/*eslint-disable*/
 
-type State = {};
 
-export class App extends React.Component<{}, State> {
-  state: State = {};
+export const App:React.FC = () => {
+  const [visibleMovies,setVisible] = useState(moviesFromServer);
+  const [query,setQuery] = useState('');
 
-  render() {
+  const filter =(inputLetters:string)=>{
+    setVisible([...moviesFromServer].filter(movie=> {
+     const { title, description } = movie;
+
+     return title.toLowerCase().includes(inputLetters.toLowerCase())
+        || description.toLowerCase().includes(inputLetters.toLowerCase());
+  }))
+}
+
     return (
       <div className="page">
         <div className="page-content">
           <div className="box">
             <div className="field">
+
               <label htmlFor="search-query" className="label">
                 Search movie
               </label>
 
               <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                />
+              <input
+                type="text"
+                value={query}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setQuery(event.currentTarget.value);
+                  filter(query);
+          }}
+        />
               </div>
             </div>
           </div>
 
-          <MoviesList movies={moviesFromServer} />
+          <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
           Sidebar goes here
         </div>
       </div>
     );
-  }
 }
