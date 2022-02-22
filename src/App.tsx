45a53880f4bcb678fcf2c4a431f93d841e812d3f
Juliya <jursylian@gmail.com>
2022-02-22 10:14:34 +0200

    delete unusefull component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
-import { Search } from './components/Search';
 import moviesFromServer from './api/movies.json';
 
 type State = {
@@ -13,9 +12,12 @@ export class App extends React.Component<{}, State> {
     query: '',
   };
 
-  onChangeFunction = (value: string): void => {
-    this.setState({ query: value });
-  };
+  changeQuery() {
+    return (
+      (event: React.ChangeEvent<HTMLInputElement>) => (
+        this.setState({ query: event.target.value }))
+    );
+  }
 
   render() {
     const { query } = this.state;
@@ -27,8 +29,23 @@ export class App extends React.Component<{}, State> {
     return (
       <div className="page">
         <div className="page-content">
-          <Search value={this.state.query} onChange={this.onChangeFunction} />
-
+          <div className="box">
+            <div className="field">
+              {/* eslint-disable-next-line */}
+              <label htmlFor="search-query" className="label">
+                Search movie
+              </label>
+              <div className="control">
+                <input
+                  type="text"
+                  id="search-query"
+                  className="input"
+                  placeholder="Type search word"
+                  onChange={this.changeQuery()}
+                />
+              </div>
+            </div>
+          </div>
           <MoviesList movies={visibleMovies} />
         </div>
         <div className="sidebar">
