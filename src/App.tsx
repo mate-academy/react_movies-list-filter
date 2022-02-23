f353a3d3f59946a6138add71c819a2af9ab3c79d
Mykhailo Ianush <yanushmisha@gmail.com>
2022-02-23 14:36:31 +0200

    change on React.FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,49 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {
-  query: string;
-};
+export const App :React.FC = () => {
+  const [query, setQuery] = useState('');
 
-export class App extends React.Component<{}, State> {
-  state: State = {
-    query: '',
-  };
-
-  changeQuery() {
-    return (
-      (event: React.ChangeEvent<HTMLInputElement>) => (
-        this.setState({ query: event.target.value }))
-    );
-  }
-
-  render() {
-    let visibleMovies = [...moviesFromServer];
-    const { query } = this.state;
-
+  const getVisibleMovies = () => {
     if (query !== '') {
-      visibleMovies = visibleMovies.filter(movie => (
+      return moviesFromServer.filter(movie => (
         movie.title.toLowerCase().includes(query.toLowerCase())
         || movie.description.toLowerCase().includes(query.toLowerCase())
       ));
     }
 
-    return (
-      <div className="page">
-        <div className="page-content">
-          <div className="box">
-            <div className="field">
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
+    return moviesFromServer;
+  };
 
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                  onChange={this.changeQuery()}
-                />
-              </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={(event) => setQuery(event?.target.value)}
+              />
             </div>
           </div>
+        </div>
 
-          <MoviesList movies={visibleMovies} />
-        </div>
-        <div className="sidebar">
-          Sidebar goes here
-        </div>
+        <MoviesList movies={getVisibleMovies()} />
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
+    </div>
+  );
+};
