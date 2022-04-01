41b550d5a1767434bf631e35b6fbff78ea39f2b8
Test <test@iMac-IMac.local>
2022-04-02 01:07:19 +0300

    last

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [movies, setFilterMovies] = useState(moviesFromServer);
+  const [text, setText] = useState('');
+
+  useEffect(() => setFilterMovies(
+    [...moviesFromServer.filter(item => item.title.includes(text))],
+  ), [text]);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -19,13 +27,15 @@ export const App: React.FC = () => {
                 type="text"
                 id="search-query"
                 className="input"
+                value={text}
+                onChange={(event) => setText(event.target.value)}
                 placeholder="Type search word"
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movies} />
       </div>
       <div className="sidebar">
         Sidebar goes here
