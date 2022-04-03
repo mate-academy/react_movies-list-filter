0a9c162ff3cb62432bd02220d6313a875c32fd7c
Roman Khemych <roman19940912@gmail.com>
2022-04-03 14:53:53 +0300

    Movie List

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const visibleMovies = moviesFromServer.filter(movie => {
+    return movie.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+    || movie.description.toLocaleLowerCase().includes(query.toLocaleLowerCase());
+  });
+
   return (
     <div className="page">
       <div className="page-content">
@@ -17,6 +28,8 @@ export const App: React.FC = () => {
             <div className="control">
               <input
                 type="text"
+                value={query}
+                onChange={handleChange}
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
@@ -25,7 +38,7 @@ export const App: React.FC = () => {
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
       <div className="sidebar">
         Sidebar goes here
