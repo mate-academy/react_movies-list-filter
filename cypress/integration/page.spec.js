1aed5ab48d235807699f5f279044ad4741fbc097
Yuriy Shvorob <yuriyshvorob@Yuriys-MacBook-Pro.local>
2022-04-03 11:40:52 +0300

    cypress tests added

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,7 +1,30 @@
-// WRITE TESTS HERE
-
-describe('Page', () => {
-  it('should be visitable', () => {
-    cy.visit('/');
-  });
+import movies from "../../src/api/movies.json"
+ 
+describe('movies list filter', () => {
+ beforeEach('visit the page', () => {
+   cy.visit('/');
+ });
+ 
+ it('search lowercase title "rogue one"', () => {
+   cy.get('#search-query').type('rogue one');
+   cy.get('.title').should('have.text', movies[3].title);
+ });
+ 
+ it('search uppercase title "ROGUE ONE"', () => {
+   cy.get('#search-query').type('ROGUE ONE');
+   cy.get('.title').should('have.text', movies[3].title);
+ })
+ 
+ it('search for coincidence in movie content', () => {
+   cy.get('#search-query').type('daring');
+   cy.get('.content').should('contain', 'daring');
+ })
+ 
+ it('search for partly coincidence', () => {
+   cy.get('#search-query').type('pla');
+   cy.get('.card').should('have.length', 3);
+   cy.get('.card')
+     .find('.content')
+     .should('contain', 'pla');
+ })
 });
