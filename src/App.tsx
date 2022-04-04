d2797618dac9f98fe9d13f97e595b46aadb4e468
Vlad Brynzya <vladbrynzya@gmail.com>
2022-04-04 15:20:53 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [findMovie, setFindMovie] = useState('');
+  const copyMovie = [...moviesFromServer];
+
+  const findYourMovie = useMemo(() => copyMovie.filter(movie => {
+    const findTitle = movie.title.toLowerCase().includes(findMovie.toLowerCase());
+    const findDescription = movie.description.toLowerCase().includes(findMovie.toLowerCase());
+    /* eslint-disable-next-line */
+    console.log(findMovie);
+
+    return findTitle || findDescription;
+  }), [copyMovie]);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +32,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={findMovie}
+                onChange={(event) => setFindMovie(event.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={findYourMovie} />
       </div>
       <div className="sidebar">
         Sidebar goes here
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
+import React from 'react';
 import { App } from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
