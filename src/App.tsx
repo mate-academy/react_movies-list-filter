9db9eec5d9867ab8d7bdac74ab7dca08571cca61
Yaslik1033 <55955258+Yaslik1033@users.noreply.github.com>
2022-04-06 09:57:21 +0300

    some message

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [searchValue, searchHandler] = useState('');
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +22,19 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={searchValue}
+                onChange={(event) => {
+                  searchHandler(event.target.value);
+                }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList
+          movies={moviesFromServer}
+          search={searchValue}
+        />
       </div>
       <div className="sidebar">
         Sidebar goes here
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -3,13 +3,26 @@ import React from 'react';
 import { MovieCard } from '../MovieCard';
 
 interface Props {
-  movies: Movie[];
+  movies: Movie[],
+  search: string,
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList: React.FC<Props> = ({ movies, search }) => {
+  const copy = [...movies];
+  let visibleMovies = [...movies];
+
+  if (search !== '') {
+    visibleMovies = copy.filter(movie => (
+      movie.title.toLowerCase().includes(search.toLowerCase())
+      || movie.description.toLowerCase().includes(search.toLowerCase())
+    ));
+  }
+
+  return (
+    <div className="movies">
+      {visibleMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
