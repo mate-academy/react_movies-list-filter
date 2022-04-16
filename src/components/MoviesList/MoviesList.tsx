b6b6a5de2c95ee2f21ba70f3b1aa45edb66126a6
Roman Kovalchuk <roma220702@gmail.com>
2022-04-16 16:48:59 +0300

    Solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -4,12 +4,22 @@ import { MovieCard } from '../MovieCard';
 
 interface Props {
   movies: Movie[];
+  value: string,
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList: React.FC<Props> = ({ movies, value }) => {
+  return (
+    <div className="movies">
+      {movies.filter(movie => {
+        if (movie.title.toLowerCase().includes(value.toLowerCase())
+          || movie.description.toLowerCase().includes(value.toLowerCase())) {
+          return movie;
+        }
+
+        return '';
+      }).map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
