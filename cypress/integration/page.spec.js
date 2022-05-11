6d36dbf5f62036f43a3be59c8b9e1970726b09d1
yuriyshvorob <zyuraz1993@gmail.com>
2022-05-11 17:44:42 +0300

    [python morning task] tests style corrections

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -6,30 +6,42 @@ describe('Page', () => {
   });
   
   it('should search lowercase title "rogue one"', () => {
-    cy.get('#search-query').type('rogue one');
-    cy.get('.title').should('have.text', movies[3].title);
-    cy.get('#search-query').type('{selectAll}{backspace}');
+    cy.get('#search-query')
+      .type('rogue one');
+    cy.get('.title')
+      .should('have.text', movies[3].title);
+    cy.get('#search-query')
+      .type('{selectAll}{backspace}');
   });
   
   it('should search uppercase title "ROGUE ONE"', () => {
-    cy.get('#search-query').type('ROGUE ONE');
-    cy.get('.title').should('have.text', movies[3].title);
-    cy.get('#search-query').type('{selectAll}{backspace}');
-  })
+    cy.get('#search-query')
+      .type('ROGUE ONE');
+    cy.get('.title')
+      .should('have.text', movies[3].title);
+    cy.get('#search-query')
+      .type('{selectAll}{backspace}');
+  });
   
   it('should search for coincidence in movie content', () => {
-    cy.get('#search-query').type('daring');
-    cy.get('.content').should('contain', 'daring');
-    cy.get('#search-query').type('{selectAll}{backspace}');
-  })
+    cy.get('#search-query')
+      .type('daring');
+    cy.get('.content')
+      .should('contain', 'daring');
+    cy.get('#search-query')
+      .type('{selectAll}{backspace}');
+  });
   
   it('should search for partly coincidence', () => {
-    cy.get('#search-query').type('pla');
-    cy.get('.card').should('have.length', 3);
+    cy.get('#search-query')
+      .type('pla');
+    cy.get('.card')
+      .should('have.length', 3);
     cy.get('.card')
       .find('.content')
       .should('contain', 'pla');
-    cy.get('#search-query').type('{selectAll}{backspace}');
-  })
+    cy.get('#search-query')
+      .type('{selectAll}{backspace}');
+  });
 
 });
