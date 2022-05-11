a875767f77cfe59aec359faf8ad2e47a41066b5d
Oksana Tyshchenko <oksanasergeevna30@gmail.com>
2022-05-11 16:43:57 +0300

    MoviesList filter was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const changeQuery = (event: React.SyntheticEvent<EventTarget>) => {
+    const { target } = event;
+    const { value } = target as HTMLInputElement;
+
+    setQuery(value);
+  };
+
+  const queryForSearch = query.toLowerCase();
+
+  const filterMovies = (movies: Movie[], searchParam: string) => {
+    return movies.filter(({ title, description }) => {
+      const changeTitle = title.toLowerCase();
+      const changeDescription = description.toLowerCase();
+
+      return changeTitle.includes(searchParam)
+      || changeDescription.includes(searchParam);
+    });
+  };
+
+  const visibleMovies = useMemo(() => filterMovies(moviesFromServer,
+    queryForSearch), [moviesFromServer, queryForSearch]);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +44,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={changeQuery}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        {visibleMovies.length > 0 && <MoviesList movies={visibleMovies} />}
       </div>
       <div className="sidebar">
         Sidebar goes here
