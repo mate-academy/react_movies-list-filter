3744fd6cef499946e9963d3fa417517f67afce4b
Pavlo Stasiv <99363993+StasivPavlo@users.noreply.github.com>
2022-05-16 15:51:26 +0200

    react_movies-list-filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { ChangeEvent, useMemo, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const visibleMovies = useMemo(() => moviesFromServer
+    .filter(movie => {
+      return (movie.title + movie.description)
+        .toLowerCase()
+        .includes(query.toLowerCase());
+    }), [query]);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -17,15 +30,18 @@ export const App: React.FC = () => {
             <div className="control">
               <input
                 type="text"
+                name="query"
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={handleChange}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
       <div className="sidebar">
         Sidebar goes here
