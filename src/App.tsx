22190a9f01d8212b964d9ca2e6d0612a726425b3
Ievgen Volkov <neoodessa86@gmail.com>
2022-05-29 23:16:59 +0300

    final Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,12 @@
-import React, {
-  ChangeEvent, useCallback,
-  useState,
-} from 'react';
+import React from 'react';
 import './App.scss';
-import { MoviesList } from './components/MoviesList';
-import { useMovies } from './hooks/useMovies';
+import { Page } from './components/Page/Page';
+import { Movies } from './components/Movies/Movies';
 
 export const App: React.FC = () => {
-  const [searchWords, setSearchWords] = useState('');
-
-  const filteredMovies = useMovies(searchWords);
-
-  const handleChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-    setSearchWords(event.target.value);
-  }, []);
-
   return (
-    <div className="page">
-      <div className="page-content">
-        <div className="box">
-          <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="search-query" className="label">
-              Search movie
-            </label>
-
-            <div className="control">
-              <input
-                type="text"
-                value={searchWords}
-                id="search-query"
-                className="input"
-                placeholder="Type search word"
-                onChange={handleChange}
-              />
-            </div>
-          </div>
-        </div>
-
-        <MoviesList movies={filteredMovies} />
-
-      </div>
-      <div className="sidebar">
-        Sidebar goes here
-      </div>
-    </div>
+    <Page>
+      <Movies />
+    </Page>
   );
 };
