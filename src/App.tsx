55d5ff2d72ab857e5210af7dee3e3947d5101a1d
Ievgen Volkov <neoodessa86@gmail.com>
2022-05-29 23:19:13 +0300

    finale Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
-import { Page } from './components/Page/Page';
-import { Movies } from './components/Movies/Movies';
+import { Page } from './components/Page';
+import { Movies } from './components/Movies';
 
 export const App: React.FC = () => {
   return (
