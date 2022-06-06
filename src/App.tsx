b783612646d4e8b2957a1de4856689bd51041f0b
Andriyk0 <andriy22221@gmail.com>
2022-06-06 18:37:54 +0300

    add search

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleMovies = () => (
+    [...moviesFromServer]
+      .filter(item => item.title.toLowerCase().includes(query.toLowerCase())
+      || item.description.toLowerCase().includes(query.toLowerCase()))
+  );
+
   return (
     <div className="page">
       <div className="page-content">
@@ -18,14 +26,19 @@ export const App: React.FC = () => {
               <input
                 type="text"
                 id="search-query"
+                name="query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(event) => (
+                  setQuery(event.target.value)
+                )}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies()} />
       </div>
       <div className="sidebar">
         Sidebar goes here
