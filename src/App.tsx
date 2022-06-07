71d7bc0a7243d02d1358074312be27e49635440f
Andriy Fesych <andriyhimself@gmail.com>
2022-06-07 23:37:50 +0300

    Solution_0.2

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,15 @@ import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
-  const [visibleMovies, setMovies] = useState(moviesFromServer);
   const [query, setQuery] = useState('');
 
-  const filterMovies = () => {
-    setMovies(moviesFromServer.filter(
-      movie => movie.description.toUpperCase().includes(query.toUpperCase())
-        || movie.title.toUpperCase().includes(query.toUpperCase()),
-    ));
+  const visibleMovies = moviesFromServer.filter(
+    movie => movie.description.toUpperCase().includes(query.toUpperCase())
+      || movie.title.toUpperCase().includes(query.toUpperCase()),
+  );
+
+  const QueryChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
   };
 
   return (
@@ -19,26 +20,20 @@ export const App: React.FC = () => {
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
-
             <div className="control">
               <input
                 type="text"
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
-                onChange={(e) => {
-                  setQuery(e.target.value);
-                  filterMovies();
-                }}
+                onChange={QueryChangeHandler}
               />
             </div>
           </div>
         </div>
-
         <MoviesList movies={visibleMovies} />
       </div>
       <div className="sidebar">
