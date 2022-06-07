c4a66a056e2cdd70e17938fb5325132deecdb897
Volodymyr <lesksv2000@gmail.com>
2022-06-07 14:00:46 +0300

    commit list filter

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -4,12 +4,20 @@ import { MovieCard } from '../MovieCard';
 
 interface Props {
   movies: Movie[];
+  query: string;
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList: React.FC<Props> = ({ movies, query }) => {
+  const lowQuery = query.toLowerCase();
+  // eslint-disable-next-line max-len
+  const visibleMovies = movies.filter(movie => movie.title.toLowerCase().includes(lowQuery)
+    || movie.description.toLowerCase().includes(lowQuery));
+
+  return (
+    <div className="movies">
+      {visibleMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
