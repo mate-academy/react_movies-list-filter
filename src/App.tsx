408b3f82a6793a8978957fb17075c6b6997505bf
Ihor Yeremenko <igoree86@gmail.com>
2022-06-09 22:03:55 +0300

    First_commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -17,6 +19,10 @@ export const App: React.FC = () => {
             <div className="control">
               <input
                 type="text"
+                value={query}
+                onChange={(event) => {
+                  setQuery(() => event.target.value);
+                }}
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
@@ -25,7 +31,10 @@ export const App: React.FC = () => {
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList
+          movies={moviesFromServer}
+          byQuery={query}
+        />
       </div>
       <div className="sidebar">
         Sidebar goes here
