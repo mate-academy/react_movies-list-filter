3d314eceeaf0b6082afd0139b9684df9c51fe35d
Swazi Kunene <kuneneswazi18@gmail.com>
2022-06-14 12:43:36 +0200

    React Movies List Filter Solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -4,12 +4,28 @@ import { MovieCard } from '../MovieCard';
 
 interface Props {
   movies: Movie[];
+  query: string;
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList: React.FC<Props> = ({ movies, query }) => {
+  const visibleMovies = movies.filter(movie => {
+    const isQueryInTitle = movie.title.toLowerCase()
+      .includes(query.toLowerCase());
+    const isQueryInDescription = movie.description.toLowerCase()
+      .includes(query.toLowerCase());
+
+    if (isQueryInTitle || isQueryInDescription) {
+      return movie;
+    }
+
+    return null;
+  });
+
+  return (
+    <div className="movies">
+      {visibleMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
