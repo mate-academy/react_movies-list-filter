e93d6738186fb8ed2c29a364e5f9f312cda98224
Anastasiia Nikita <anastasiya.folk@gmail.com>
2022-06-15 16:51:26 +0300

    Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,8 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-function getVisibleMovie(movies: Movie[], query: string) {
-  const lowerQuery = query.toLowerCase();
-
-  const filteredMovie = movies.filter(movie => (
-    movie.title.toLowerCase().includes(lowerQuery)
-    || movie.description.toLowerCase().includes(lowerQuery)
-  ));
-
-  return filteredMovie;
-}
-
 export const App: React.FC = () => {
   const [query, setQuery] = useState('');
-  const visibleMovies = getVisibleMovie([...moviesFromServer], query);
 
   return (
     <div className="page">
@@ -43,7 +31,7 @@ export const App: React.FC = () => {
           </div>
         </div>
 
-        <MoviesList movies={visibleMovies} />
+        <MoviesList movies={moviesFromServer} query={query} />
       </div>
       <div className="sidebar">
         Sidebar goes here
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -4,12 +4,28 @@ import { MovieCard } from '../MovieCard';
 
 interface Props {
   movies: Movie[];
+  query: string;
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+function getVisibleMovie(movies: Movie[], query: string) {
+  const lowerQuery = query.toLowerCase();
+
+  const filteredMovie = movies.filter(movie => (
+    movie.title.toLowerCase().includes(lowerQuery)
+    || movie.description.toLowerCase().includes(lowerQuery)
+  ));
+
+  return filteredMovie;
+}
+
+export const MoviesList: React.FC<Props> = ({ movies, query }) => {
+  const visibleMovies = getVisibleMovie(movies, query);
+
+  return (
+    <div className="movies">
+      {visibleMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
