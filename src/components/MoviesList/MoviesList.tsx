a1ea336a87d91bdd905cebb6fe8544b196aeb4b0
Lilia Mazur <liliamazur2508@gmail.com>
2022-06-17 16:02:36 +0300

    solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -4,12 +4,29 @@ import { MovieCard } from '../MovieCard';
 
 interface Props {
   movies: Movie[];
+  query: string;
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+function FilterMovies(movies: Movie[], query: string) {
+  const loverCaseQuery = query.toLowerCase();
+
+  return movies.filter(movie => (
+    movie.title.toLowerCase().includes(loverCaseQuery)
+    || movie.description.toLowerCase().includes(loverCaseQuery)
+  ));
+}
+
+export const MoviesList: React.FC<Props> = ({ movies, query }) => {
+  const visibleMovies = FilterMovies(movies, query);
+
+  return (
+    <div className="movies">
+      {visibleMovies.map(movie => (
+        <MovieCard
+          key={movie.imdbId}
+          movie={movie}
+        />
+      ))}
+    </div>
+  );
+};
