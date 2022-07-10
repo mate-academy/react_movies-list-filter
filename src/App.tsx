81941ce3fdc8443df0930a9db174cfc516aa5122
Dima Cherednyk <dicherednyk@gmail.com>
2022-07-10 12:10:09 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const filterQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const input = event.currentTarget.value;
+
+    setQuery(() => {
+      return input;
+    });
+  };
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +30,16 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={filterQuery}
               />
             </div>
           </div>
         </div>
-
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList
+          movies={moviesFromServer}
+          query={query}
+        />
       </div>
       <div className="sidebar">
         Sidebar goes here
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -4,12 +4,24 @@ import { MovieCard } from '../MovieCard';
 
 interface Props {
   movies: Movie[];
+  query: string;
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList: React.FC<Props> = ({ movies, query }) => {
+  const visibleMovies = movies.filter(
+    movie => movie.title.toLowerCase().includes(query.toLowerCase())
+    || movie.description.toLowerCase().includes(query.toLowerCase()),
+  );
+
+  return (
+    <div className="movies">
+      {query.length > 0
+        ? visibleMovies.map(movie => (
+          <MovieCard key={movie.imdbId} movie={movie} />
+        ))
+        : movies.map(movie => (
+          <MovieCard key={movie.imdbId} movie={movie} />
+        ))}
+    </div>
+  );
+};
