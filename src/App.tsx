ab6a618a47e7571e098aa99fc41606f5b71eb07b
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2022-07-10 18:28:51 +0300

    Movies list filter added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const handleQueryChange = (value: string) => {
+    setQuery(value);
+  };
+
+  const moviesArr = moviesFromServer.filter(movie => {
+    return movie.title.toLowerCase().includes(query.toLowerCase())
+      || movie.description.toLowerCase().includes(query.toLowerCase());
+  });
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -20,12 +30,15 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={({ target }) => {
+                  handleQueryChange(target.value);
+                }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={moviesArr} />
       </div>
       <div className="sidebar">
         Sidebar goes here
