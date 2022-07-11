130f3e711955c10c0acd54bed8bfac273d32bac4
mira1zu <mira1zu733@gmail.com>
2022-07-11 15:12:03 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './App.scss';
+
 import { MoviesList } from './components/MoviesList';
+
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleMovies = moviesFromServer.filter((movie) => {
+    const regexp = new RegExp(query, 'i');
+
+    return regexp.test(movie.title) || regexp.test(movie.description);
+  });
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +31,15 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={(({ target }) => (
+                  setQuery(target.value)
+                ))}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
       <div className="sidebar">
         Sidebar goes here
