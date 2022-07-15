790bc71c1442ee42e758ae97d8e3127e90859bcd
Pavlo <Pmoroz2004@gmail.com>
2022-07-15 13:52:17 +0300

    my solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [search, setSearh] = useState('');
+  const visibleMovies = [...moviesFromServer]
+    .filter(movie => (
+      movie.title.toLowerCase().startsWith(search.toLowerCase())
+      || movie.description.toLowerCase().startsWith(search.toLowerCase())
+    ));
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -20,12 +26,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={search}
+                onChange={event => setSearh(event.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
       <div className="sidebar">
         Sidebar goes here
