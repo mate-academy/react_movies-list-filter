54a5df9210282a86a80f10f1bde602e80c20a64d
Oleksandr Kapustei <angelion541@gmail.com>
2022-07-15 11:00:44 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import { FC, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App: React.FC = () => {
+export const App: FC = () => {
+  const [SearchedText, setSearchedText] = useState('');
+  const visibleMovies = moviesFromServer.filter(
+    ({ title, description }) => (
+      title.toLocaleLowerCase().includes(SearchedText)
+      || description.toLocaleLowerCase().includes(SearchedText)
+    ),
+  );
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -20,12 +27,17 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={
+                  ({ target }) => setSearchedText(
+                    () => target.value.toLocaleLowerCase(),
+                  )
+                }
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
       <div className="sidebar">
         Sidebar goes here
