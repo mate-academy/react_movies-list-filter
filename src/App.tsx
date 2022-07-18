8b24c70721aacd41812fb953ef580f4670820b6e
Kate Khon <katerynakhon@gmail.com>
2022-07-18 20:10:48 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const filteredMovies = (movies: Movie[]) => {
+    if (query) {
+      const lowCaseQuery = query.toLowerCase();
+
+      return movies.filter(movie => (
+        movie.title.toLowerCase().includes(lowCaseQuery)
+        || movie.description.toLowerCase().includes(lowCaseQuery)
+      ));
+    }
+
+    return movies;
+  };
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -20,12 +38,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={handleChange}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={filteredMovies(moviesFromServer)} />
       </div>
 
       <div className="sidebar">
