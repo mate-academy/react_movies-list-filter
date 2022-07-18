3b79f285f917544b69c09d83b252b206e69cfb6d
Ihor Shynkar <aedricson@gmail.com>
2022-07-18 16:32:55 +0300

    implemented movies filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+function filterMovies(movies: Movie[], movieName: string) {
+  return movies.filter(
+    movie => (movie.title.toLowerCase().includes(movieName.toLowerCase())
+    || (movie.description.toLowerCase().includes(movieName.toLowerCase()))),
+  );
+}
+
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = [...moviesFromServer];
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +30,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={filterMovies(visibleMovies, query)} />
       </div>
 
       <div className="sidebar">
