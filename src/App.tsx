55dc26261b19b382d6a341c7d16a4c745d973c6e
Luidmyla Omelchenko <lui.omelika@gmail.com>
2022-07-19 12:10:11 +0300

    finished movies filter in query

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setSearch] = useState('');
+
+  const visibleMovies = (moviesFromServer.filter((movie) => {
+    const { title, description } = movie;
+
+    return title.toLowerCase().includes(query.toLowerCase())
+      || description.toLowerCase().includes(query.toLowerCase());
+  }));
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +29,15 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={(event) => {
+                  setSearch(event.target.value.trim());
+                }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
