125fc90d657814c31f8e52603334b2d9e343d5bf
Oleg Vetrov <oleg94038@gmail.com>
2022-08-10 15:05:08 +0300

    Added search for movies by title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const sortedMoviesList = moviesFromServer.filter(movie => {
+    return movie.title.toLowerCase().includes(searchQuery.toLowerCase());
+  });
+
   return (
     <div className="page">
       <div className="page-content">
@@ -19,13 +24,17 @@ export const App: React.FC = () => {
                 type="text"
                 id="search-query"
                 className="input"
+                value={searchQuery}
+                onChange={(event) => (
+                  setSearchQuery(event.target.value)
+                )}
                 placeholder="Type search word"
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={sortedMoviesList} />
       </div>
 
       <div className="sidebar">
