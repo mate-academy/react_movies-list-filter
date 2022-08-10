51e101a46f87fac3d6103a253ea52a3d8d7c97d2
Denys Korotkoruchko <denenergii@gmail.com>
2022-08-10 19:40:57 +0300

    ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [value, setValue] = useState('');
+
+  // eslint-disable-next-line no-console
+  console.log(value);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +25,16 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={value}
+                onChange={(event) => {
+                  setValue(event.target.value);
+                }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={moviesFromServer} value={value} />
       </div>
 
       <div className="sidebar">
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -4,12 +4,15 @@ import { MovieCard } from '../MovieCard';
 
 interface Props {
   movies: Movie[];
+  value: string;
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
+export const MoviesList: React.FC<Props> = ({ movies, value }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
+      (movie.title.toLowerCase().includes(value.toLocaleLowerCase())
+      || movie.description.toLowerCase().includes(value.toLocaleLowerCase()))
+        && <MovieCard key={movie.imdbId} movie={movie} />
     ))}
   </div>
 );
