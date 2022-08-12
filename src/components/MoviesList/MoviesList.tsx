bd44234819c6d3397e40bf8920bee659634b4937
Denys Korotkoruchko <denenergii@gmail.com>
2022-08-12 13:02:33 +0300

    add function visibleMovies

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
+interface Movie {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string
+}
+
 interface Props {
   movies: Movie[];
   value: string;
 }
 
+function visibleMovies(item:Movie, input:string) {
+  return item.title.toLowerCase().includes(input.toLocaleLowerCase())
+    || item.description.toLowerCase().includes(input.toLocaleLowerCase());
+}
+
 export const MoviesList: React.FC<Props> = ({ movies, value }) => (
   <div className="movies">
     {movies.map(movie => (
-      (movie.title.toLowerCase().includes(value.toLocaleLowerCase())
-      || movie.description.toLowerCase().includes(value.toLocaleLowerCase()))
+      visibleMovies(movie, value)
         && <MovieCard key={movie.imdbId} movie={movie} />
     ))}
   </div>
