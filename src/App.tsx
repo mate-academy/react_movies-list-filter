d3a34a136dd71d366201e71e73e8d3130f409c06
silchyn <ivan.silchin@gmail.com>
2022-08-12 12:42:53 +0300

    style correction

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-const filterMovies = (query: string) => {
-  return () => moviesFromServer.filter(movie => movie.title.toLowerCase()
-    .includes(query.toLowerCase())
-      || movie.description.toLowerCase()
-        .includes(query.toLowerCase()));
-};
+const filterMovies = (query: string) => moviesFromServer
+  .filter(movie => {
+    const titleLower = movie.title.toLowerCase();
+    const descriptionLower = movie.description.toLowerCase();
+    const queryLower = query.toLowerCase();
+
+    const titleIncludes = titleLower.includes(queryLower);
+    const descriptionIncludes = descriptionLower.includes(queryLower);
+
+    return titleIncludes || descriptionIncludes;
+  });
 
 export const App: React.FC = () => {
   const [query, setQuery] = useState('');
 
-  const visibleMovies = useMemo(filterMovies(query), [query]);
+  const visibleMovies = useMemo(() => filterMovies(query), [query]);
 
   return (
     <div className="page">
