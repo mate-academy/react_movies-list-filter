347802e0604a352d3861d91f469c3df77abcc2ba
Yevhen Nikolaienko <illnino380@gmail.com>
2022-08-12 23:36:47 +0300

    implemented movie list filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+interface Movie {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+}
+
+const getFilteredMovieList = (movieList: Movie[], query: string): Movie[] => {
+  return movieList.filter(movie => {
+    const movieName = movie.title.toLowerCase();
+    const movieDescription = movie.description.toLowerCase();
+    const movieQuery = query.toLowerCase();
+
+    return (movieName.includes(movieQuery)
+      || movieDescription.includes(movieQuery));
+  });
+};
+
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleMovies = getFilteredMovieList(moviesFromServer, query);
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -20,12 +42,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
