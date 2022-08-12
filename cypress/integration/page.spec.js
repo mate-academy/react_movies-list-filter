96fad8c80d89cc61552c75797504128d776ea0d7
mhrynko <glukym@gmail.com>
2022-08-12 16:35:30 +0300

    improve tests

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,46 +1,161 @@
-import movies from "../../src/api/movies.json"
+const page = {
+  searchField: () => cy.get('#search-query'),
+  movies: () => cy.get('.card'),
+}
 
 describe('Page', () => {
   beforeEach(() => {
     cy.visit('/');
   });
-  
-  it('should search lowercase title "rogue one"', () => {
-    cy.get('#search-query')
-      .type('rogue one');
-    cy.get('.title')
-      .should('have.text', movies[3].title);
-    cy.get('#search-query')
-      .type('{selectAll}{backspace}');
+
+  it('should have an empty search field', () => {
+    page.searchField()
+      .should('have.value', '');
+  });
+
+  it('should show all the movies by default', () => {
+    page.movies()
+      .should('have.length', 5);
+
+    page.movies().eq(0)
+      .find('.title')
+      .should('have.text', 'Inception');
+
+    page.movies().eq(4)
+      .find('.title')
+      .should('have.text', 'The Holiday');
+  });
+
+  it('should allow to enter some text', () => {
+    page.searchField()
+      .type('Hello')
+      .should('have.value', 'Hello');
+  });
+
+  it('should allow to remove entered text', () => {
+    page.searchField()
+      .type('Hello')
+      .type('{selectAll}{backspace}')
+      .should('have.value', '');
   });
   
-  it('should search uppercase title "ROGUE ONE"', () => {
-    cy.get('#search-query')
-      .type('ROGUE ONE');
-    cy.get('.title')
-      .should('have.text', movies[3].title);
-    cy.get('#search-query')
-      .type('{selectAll}{backspace}');
+  it('should search by exact title', () => {
+    page.searchField()
+      .type('Love Actually');
+
+    page.movies()
+      .should('have.length', 1)
+      .eq(0)
+      .find('.title')
+      .should('have.text', 'Love Actually');
   });
-  
-  it('should search for a match in movie content', () => {
-    cy.get('#search-query')
-      .type('daring');
-    cy.get('.content')
-      .should('contain', 'daring');
-    cy.get('#search-query')
-      .type('{selectAll}{backspace}');
+
+  it('should search by a start of a title', () => {
+    page.searchField()
+      .type('Rogue');
+
+    page.movies()
+      .should('have.length', 1)
+      .eq(0)
+      .find('.title')
+      .should('have.text', 'Rogue One');
   });
-  
-  it('should search partial match', () => {
-    cy.get('#search-query')
-      .type('pla');
-    cy.get('.card')
-      .should('have.length', 3);
-    cy.get('.card')
-      .find('.content')
-      .should('contain', 'pla');
-    cy.get('#search-query')
-      .type('{selectAll}{backspace}');
+
+  it('should search by a part of a title', () => {
+    page.searchField()
+      .type('Day After');
+    
+    page.movies()
+      .should('have.length', 1)
+      .eq(0)
+      .find('.title')
+      .should('have.text', 'The Day After Tomorrow');
+  });
+
+  it('should search by a lower case part of a title', () => {
+    page.searchField()
+      .type('day after');
+    
+    page.movies()
+      .should('have.length', 1)
+      .eq(0)
+      .find('.title')
+      .should('have.text', 'The Day After Tomorrow');
+  });
+
+  it('should search by an upper case part of a title', () => {
+    page.searchField()
+      .type('DAY AFTER');
+    
+    page.movies()
+      .should('have.length', 1)
+      .eq(0)
+      .find('.title')
+      .should('have.text', 'The Day After Tomorrow');
+  });
+
+  it('should search by a mixed case part of a title', () => {
+    page.searchField()
+      .type('DaY aFTer');
+    
+    page.movies()
+      .should('have.length', 1)
+      .eq(0)
+      .find('.title')
+      .should('have.text', 'The Day After Tomorrow');
+  });
+
+  it('should search by a part of a description', () => {
+    page.searchField()
+      .type('interrelated tales');
+    
+    page.movies()
+      .should('have.length', 1)
+      .eq(0)
+      .find('.title')
+      .should('have.text', 'Inception');
+  });
+
+  it('should search by a mixed case part of a description', () => {
+    page.searchField()
+      .type('interRELATED taLES');
+    
+    page.movies()
+      .should('have.length', 1)
+      .eq(0)
+      .find('.title')
+      .should('have.text', 'Inception');
+  });
+
+  it('should show all the matched movies', () => {
+    page.searchField()
+      .type('love');
+    
+    page.movies().should('have.length', 3)
+      
+    page.movies().eq(0).find('.title').should('have.text', 'Inception');
+    page.movies().eq(1).find('.title').should('have.text', 'Love Actually');
+    page.movies().eq(2).find('.title').should('have.text', 'The Holiday');
+  });
+
+  it('should update movies on type', () => {
+    page.searchField().type('love');
+    page.movies().should('have.length', 3);
+
+    page.searchField().type(' ');
+    page.movies().should('have.length', 2);
+  });
+
+  it('should show no movies if query does not match any title or description', () => {
+    page.searchField().type('loves');
+    page.movies().should('not.exist');
+  });
+
+  it('should restore movies after if the query starts matching', () => {
+    page.searchField().type('loves');
+    page.movies().should('not.exist');
+
+    page.searchField().type('{backspace}');
+    page.movies().should('have.length', 3);
   });
 });
