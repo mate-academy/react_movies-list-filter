d6a2ea6dad62de5fcff9f541aadfcd40c75eb6d8
Volodymyr Dolishnii <leedeelee3@gmail.com>
2022-08-14 17:18:27 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './App.scss';
-import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import { MoviesList } from './components/MoviesList';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleMovies = moviesFromServer.filter(
+    movie => movie.title
+      .toLowerCase()
+      .includes(query.toLowerCase())
+    || movie.description
+      .toLowerCase()
+      .includes(query.toLowerCase()),
+  );
+
   return (
     <div className="page">
       <div className="page-content">
-        <div className="box">
-          <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="search-query" className="label">
-              Search movie
-            </label>
+        <div className="search-bar">
+          <input
+            id="search-query"
+            type="text"
+            className="input-field"
+            placeholder="Search..."
+            value={query}
+            onChange={(event) => {
+              const { value } = event.target;
 
-            <div className="control">
-              <input
-                type="text"
-                id="search-query"
-                className="input"
-                placeholder="Type search word"
-              />
-            </div>
-          </div>
+              setQuery(value);
+            }}
+          />
         </div>
-
-        <MoviesList movies={moviesFromServer} />
+        {visibleMovies.length > 0
+          ? <MoviesList movies={visibleMovies} />
+          : 'Nothing found'}
       </div>
 
       <div className="sidebar">
-        Sidebar goes here
+        Sidebar will be here
       </div>
     </div>
   );
