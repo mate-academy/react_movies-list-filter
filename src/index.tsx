cc23fbdf14a692d90cea2b825aba833b6a30a331
Elina Kovalenko <kovalenkoelin@gmail.com>
2022-08-14 21:04:35 +0300

    attempt

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
 import { App } from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root')) ;
