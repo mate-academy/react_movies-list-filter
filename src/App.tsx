4247aee7d0e1c5a8096c922c4a70cb5a7afa3f65
Andriy Kachalkin <kachalkin1306@gmail.com>
2022-09-11 19:59:41 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,71 @@
-import React from 'react';
+import React, { BaseSyntheticEvent } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App: React.FC = () => {
-  return (
-    <div className="page">
-      <div className="page-content">
-        <div className="box">
-          <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="search-query" className="label">
-              Search movie
-            </label>
+function lowerIncluder(state1: string, state2: string): boolean {
+  return state1.toLowerCase().includes(state2.toLowerCase());
+}
 
-            <div className="control">
-              <input
-                type="text"
-                id="search-query"
-                className="input"
-                placeholder="Type search word"
-              />
+interface Movie {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+}
+
+export class App extends React.Component {
+  state = {
+    query: '',
+  };
+
+  setValue = (input: BaseSyntheticEvent) => {
+    this.setState({ query: input.target.value });
+  };
+
+  filterList = (list: Movie[]) => {
+    const { query } = this.state;
+    const filteredList = [...list];
+
+    return filteredList.filter(({ title, description }) => (
+      lowerIncluder(title, query)
+      || lowerIncluder(description, query)
+    ));
+  };
+
+  render() {
+    const filteredList = this.filterList(moviesFromServer);
+
+    return (
+      <div className="page">
+        <div className="page-content">
+          <div className="box">
+            <div className="field">
+              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+              <label htmlFor="search-query" className="label">
+                Search movie
+              </label>
+
+              <div className="control">
+                <input
+                  type="text"
+                  id="search-query"
+                  className="input"
+                  placeholder="Type search word"
+                  onChange={this.setValue}
+                />
+              </div>
             </div>
           </div>
+
+          <MoviesList movies={filteredList} />
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <div className="sidebar">
+          Sidebar goes here
+        </div>
       </div>
-
-      <div className="sidebar">
-        Sidebar goes here
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
