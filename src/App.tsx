9702ae64b90e4fab7023865026b18eed258a1ceb
Andriy Kachalkin <kachalkin1306@gmail.com>
2022-09-11 20:15:55 +0300

    remade with functional component and useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,54 @@
-import React, { BaseSyntheticEvent } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-function lowerIncluder(state1: string, state2: string): boolean {
+function lowerCompare(state1: string, state2: string): boolean {
   return state1.toLowerCase().includes(state2.toLowerCase());
 }
 
-interface Movie {
-  title: string;
-  description: string;
-  imgUrl: string;
-  imdbUrl: string;
-  imdbId: string;
-}
+export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
 
-export class App extends React.Component {
-  state = {
-    query: '',
-  };
-
-  setValue = (input: BaseSyntheticEvent) => {
-    this.setState({ query: input.target.value });
-  };
-
-  filterMovies = (list: Movie[]) => {
-    const { query } = this.state;
-    const filteredList = [...list];
-
-    return filteredList.filter(({ title, description }) => (
-      lowerIncluder(title, query)
-      || lowerIncluder(description, query)
+  const visibleMovies = [...moviesFromServer]
+    .filter((movie) => (
+      lowerCompare(movie.description, query)
+      || lowerCompare(movie.title, query)
     ));
+
+  const querySetter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
   };
 
-  render() {
-    const visibleMovies = this.filterMovies(moviesFromServer);
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
 
-    return (
-      <div className="page">
-        <div className="page-content">
-          <div className="box">
-            <div className="field">
-              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
-
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                  onChange={this.setValue}
-                />
-              </div>
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={query}
+                onChange={querySetter}
+              />
             </div>
           </div>
-
-          <MoviesList movies={visibleMovies} />
         </div>
 
-        <div className="sidebar">
-          Sidebar goes here
-        </div>
+        <MoviesList movies={visibleMovies} />
       </div>
-    );
-  }
-}
+
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
+    </div>
+  );
+};
