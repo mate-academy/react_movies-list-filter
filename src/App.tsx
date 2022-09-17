b521d10a48d105568c73ed2490f872f1c78969b0
Vadls <vadim_tanasienko54541@ukr.net>
2022-09-17 16:58:25 +0300

    Implement working search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const handleOnChange = ({ target }: { target: HTMLInputElement }) => {
+    const { value } = target;
+
+    setQuery(value);
+  };
+
+  const visibleMovies = moviesFromServer.filter(movie => {
+    const lowerTitle = movie.title.toLowerCase();
+    const lowerDescription = movie.description.toLowerCase();
+    const lowerQuery = query.toLowerCase();
+
+    return (lowerTitle.includes(lowerQuery)
+    || lowerDescription.includes(lowerQuery));
+  });
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +37,15 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                name="text"
+                value={query}
+                onChange={handleOnChange}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
