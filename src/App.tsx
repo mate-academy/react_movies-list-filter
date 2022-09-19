ce0a24cdd844bcdec460f4262791e1b718ea4def
Anatolii Bukoros <bukoros.anatoliy@gmail.com>
2022-09-19 23:32:49 +0300

    created react movie list filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import { Movie } from './components/types/Movie';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  function filtreMoviesList(movie: Movie): boolean {
+    const { title, description } = movie;
+    const queryToDown = query.toLocaleLowerCase();
+    const titleToDown = title.toLocaleLowerCase();
+    const descriptionToDown = description.toLocaleLowerCase();
+
+    if (titleToDown.includes(queryToDown)
+    || descriptionToDown.includes(queryToDown)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  const filtredMovies = moviesFromServer.filter(filtreMoviesList);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +39,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(event) => (setQuery(event.target.value))}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={filtredMovies} />
       </div>
 
       <div className="sidebar">
