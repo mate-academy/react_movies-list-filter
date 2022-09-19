19e97dc4689545d3f52c700ff4f9fa88d1c05709
Zinchenko Anastasiia <zincenkonasta6@gmail.com>
2022-09-19 16:49:43 +0300

    movies list solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+// import { event } from 'cypress/types/jquery';
+
+export function getVisibleMovies(movies: Movie[], query: string) {
+  const visibleMovies = [...movies];
+
+  return visibleMovies.filter(movie => {
+    const { title, description } = movie;
+
+    return (
+      title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+      || description.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+    );
+  });
+}
 
 export const App: React.FC = () => {
+  const [query, setMovie] = useState('');
+  const visibleMovies = getVisibleMovies(moviesFromServer, query);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +37,13 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={(event) => setMovie(event.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
