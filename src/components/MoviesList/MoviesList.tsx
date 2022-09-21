f19b81be5c829f1dadbe264514e6806ecb0633a5
Max Kravchenko <max.kravchenko@gmail.com>
2022-09-21 13:55:27 +0300

    Make React Movie List Filter

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -4,12 +4,26 @@ import { MovieCard } from '../MovieCard';
 
 interface Props {
   movies: Movie[];
+  searchWord: string;
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList: React.FC<Props> = ({ movies, searchWord }) => {
+  const query = searchWord.toLowerCase();
+  const visibleMovies = movies
+    .filter(
+      (movie) => movie.title.toLowerCase().includes(query)
+        || movie.description.toLowerCase().includes(query),
+    );
+
+  return (
+    <div className="movies">
+      {visibleMovies
+        ? (visibleMovies).map(movie => (
+          <MovieCard key={movie.imdbId} movie={movie} />
+        ))
+        : (movies).map(movie => (
+          <MovieCard key={movie.imdbId} movie={movie} />
+        ))}
+    </div>
+  );
+};
