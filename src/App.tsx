a42e9e6e0170fa6a380e3a7672aceaac3f489753
Shmelyov Oleg <shmelyov.oleg13@gmail.com>
2022-09-22 08:02:35 +0300

    add: search filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+export function checkQuery(query:string, ...contentBlocks:string[]) {
+  return (contentBlocks.map(content => content).join('').toLowerCase())
+    .includes(query.toLowerCase());
+}
+
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleMovies = moviesFromServer
+    .filter(({ title, description }) => (
+      checkQuery(query, title, description)
+    ));
+
+  const handleChange = (
+    event:React.ChangeEvent<HTMLInputElement>,
+  ) => (
+    setQuery(event.target.value));
+
   return (
     <div className="page">
       <div className="page-content">
@@ -13,19 +30,19 @@ export const App: React.FC = () => {
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
-
             <div className="control">
               <input
                 type="text"
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={handleChange}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
