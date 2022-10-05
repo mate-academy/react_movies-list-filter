3e21fe57e900a806ec6d1edb3e9ccd15c2224296
Tilak Kumarclear <mailtotilakkumar@gmail.com>
2022-10-05 20:27:15 +0530

    sol

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = [...moviesFromServer];
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
             {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="search-query" className="label">
+            <label
+              htmlFor="search-query"
+              className="label"
+            >
               Search movie
             </label>
 
@@ -20,17 +26,25 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={
+          query
+            ? visibleMovies.filter(
+              m => m.title.toLowerCase().includes(query.toLowerCase())
+              || m.description.toLowerCase().includes(query.toLowerCase()),
+            )
+            : visibleMovies
+        }
+        />
       </div>
 
-      <div className="sidebar">
-        Sidebar goes here
-      </div>
+      <div className="sidebar">Sidebar goes here</div>
     </div>
   );
 };
