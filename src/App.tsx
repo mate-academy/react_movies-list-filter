a7561fbdded4597f4f46279e53fec3e20740955e
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-10-22 15:15:49 +0800

    version2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
   // const [movies, setMovies] = useState(moviesFromServer);
+  const [input, setInput] = useState('');
+  // const [username, setUsername] = useState('');
+  // const [password, setPassword] = useState('');
+
+  const handleChange = (event: any) => {
+    // setInput(event.target.value.trim());
+    const { value } = event.target;
+
+    setInput(value);
+
+    //   setInput(type === 'checkbox'
+    //   ? checked
+    //   : value,
+    // );
+
+    console.log(input);
+  };
+
+  // const handleSubmit = (event) => {
+  //   setInput(event.target.value.trim());
+  //   console.log(input);
+  // };
+
   return (
     <div className="page">
       <div className="page-content">
@@ -21,7 +44,46 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={input}
+                // defaultValue={input}
+                // onChange={(event) => {
+                // setInput(event.target.value.trim());
+                // console.log(event.target.value);
+                // console.log(input);
+                // }}
+                onChange={handleChange}
               />
+
+              {/* <form onSubmit={(event) => {
+                event.preventDefault();
+                console.log(password, username)
+              }}
+              >
+                <input
+                  type="text"
+                  placeholder="username"
+                  value={username}
+                  // onChange={(event) => {
+                  //   setUsername(event.target.value);
+                  // }}
+                  onChange={handleSubmit}
+                />
+                <input
+                  type="password"
+                  placeholder="password"
+                  value={password}
+                  // onChange={(event) => {
+                  //   setPassword(event.target.value);
+                  // }}
+                  onChange={handleSubmit}
+
+                />
+                <button
+                  type="submit"
+                >
+                  click here
+                </button>
+              </form> */}
             </div>
           </div>
         </div>
