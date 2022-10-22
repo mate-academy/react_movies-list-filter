6fc464342eaacf5ab380443ec6769380bb00ef47
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-10-22 12:52:19 +0800

    version1

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  // const [movies, setMovies] = useState(moviesFromServer);
   return (
     <div className="page">
       <div className="page-content">
