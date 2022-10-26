735934eff504a8f01bc6bd25e5bb7ef726e72793
Artur Nagaycev <artur.nagaycev@gmail.com>
2022-10-26 16:43:27 +0300

    implemented movie filtering by title and description

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const changeQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const visibleMovies = moviesFromServer.filter(({ title, description }) => {
+    const lowerTitle = title.toLowerCase();
+    const lowerDescription = description.toLowerCase();
+    const lowerQuery = query.toLowerCase();
+
+    return lowerTitle.includes(lowerQuery)
+      || lowerDescription.includes(lowerQuery);
+  });
+
   return (
     <div className="page">
       <div className="page-content">
@@ -19,13 +33,15 @@ export const App: React.FC = () => {
                 type="text"
                 id="search-query"
                 className="input"
+                value={query}
                 placeholder="Type search word"
+                onChange={changeQuery}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
