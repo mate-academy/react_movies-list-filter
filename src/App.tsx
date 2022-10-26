4f36b2316f120c77dc86b3a180c56d0709753e32
Ilya Maksymov <ivashthestampede01@gmail.com>
2022-10-26 16:46:04 +0300

    Implement rendering filtered movies list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import { FC, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App: React.FC = () => {
+export const App: FC = () => {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (newValue: string) => {
+    setQuery(newValue);
+  };
+
+  const visibleMovies = moviesFromServer
+    .filter(movie => (
+      movie.title.toLowerCase().includes(query.toLowerCase())
+        || movie.description.toLowerCase().includes(query.toLowerCase())
+    ));
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +32,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(e) => handleInputChange(e.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
