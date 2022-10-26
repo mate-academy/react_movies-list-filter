5746058864f8352d650c45db1ec375f4d54af996
graift-off <graf.off.it@gmail.com>
2022-10-26 22:53:41 +0300

    feat: movies filter comp was implemented by useState & useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const [visibleMovies, setVisibleMovies] = useState<Movie[]>(moviesFromServer);
+
+  useEffect(() => {
+    const loweredQuery = query.toLowerCase();
+    const movies = moviesFromServer.filter(({ title, description }) => {
+      const loweredTitle = title.toLowerCase();
+      const loweredDescription = description.toLowerCase();
+
+      return (loweredTitle.includes(loweredQuery)
+        || loweredDescription.includes(loweredQuery));
+    });
+
+    setVisibleMovies(movies);
+  }, [query]);
+
+  const handleChane = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -19,13 +38,15 @@ export const App: React.FC = () => {
                 type="text"
                 id="search-query"
                 className="input"
+                value={query}
+                onChange={handleChane}
                 placeholder="Type search word"
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
