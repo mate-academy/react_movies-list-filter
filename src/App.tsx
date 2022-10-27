4da4fb215cee126ac6bf4dbbc9c8744ed857f330
Anton Iskryk <iskryk.anton@gmail.com>
2022-10-27 17:37:29 +0300

    Implement case insensitive film search

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleMovies = moviesFromServer.filter(movie => {
+    const { title, description } = movie;
+
+    const lowerCaseTitle = title.toLowerCase();
+    const lowerCaseDescription = description.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+
+    return lowerCaseTitle.includes(lowerCaseQuery)
+      || lowerCaseDescription.includes(lowerCaseQuery);
+  });
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +33,16 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(event) => {
+                  setQuery(event.target.value);
+                }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
