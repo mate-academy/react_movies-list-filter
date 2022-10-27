776be2ea93fc508c79b3fd22b35bdb47897d3d1b
Andriy Hladkevych <andriyglad@gmail.com>
2022-10-27 13:31:07 +0300

    Implement search bar for movies list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const defaultMoviesList = moviesFromServer;
+  const [query, setQuery] = useState('');
+  const [visibleMovies, setVisibleList] = useState(defaultMoviesList);
+
+  const filterMoviesList = (filter: string) => {
+    setVisibleList(defaultMoviesList.filter(movie => (
+      movie.title.toLowerCase().includes(filter.toLowerCase())
+        || movie.description.toLowerCase().includes(filter.toLowerCase())
+    )));
+  };
+
+  useEffect(() => {
+    filterMoviesList(query);
+  }, [query]);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +35,16 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(event) => {
+                  setQuery(event.target.value);
+                }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
