7a3efbe37550c3159cc92294579e5a33a7bec887
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2022-10-28 17:22:15 +0300

    Created variables in the getMatchingMovies function and implemented optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.scss';
+import { debounce } from 'lodash';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-const filtrateMovies = (movies: Movie[], query: string) => {
-  return movies.filter(movie => (
-    movie.description.toLowerCase().includes(query.toLowerCase())
-    || movie.title.toLowerCase().includes(query.toLowerCase())
-  ));
+const getMatchingMovies = (query: string): Movie[] => {
+  return moviesFromServer.filter(({ title, description }) => {
+    const lowerCaseQuery = query.toLowerCase();
+    const matchTitle = title.toLowerCase().includes(lowerCaseQuery);
+    const matchDescription = description.toLowerCase().includes(lowerCaseQuery);
+
+    return matchTitle || matchDescription;
+  });
 };
 
 export const App: React.FC = () => {
   const [query, setQuery] = useState('');
-  const visibleMovies = filtrateMovies(moviesFromServer, query);
+  const [appliedQuery, setAppliedQuery] = useState('');
+
+  const visibleMovies: Movie[] = useMemo(() => (
+    getMatchingMovies(query)
+  ), [moviesFromServer, appliedQuery]);
+
+  const applyQuery = useCallback(
+    debounce(setAppliedQuery, 1000),
+    [],
+  );
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
+    applyQuery(event.target.value);
   };
 
   return (
