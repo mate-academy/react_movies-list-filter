d83f5b92ea229df06ae9b67c40b18e6c2fed3d41
poznianski <poznianski@gmail.com>
2022-10-30 20:25:31 +0200

    implemented live filter for movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const isQueryMatch = (value: string) => {
+    return value.toLowerCase().includes(query.toLowerCase());
+  };
+
+  const visibleMovies = moviesFromServer.filter(({ description, title }) => (
+    isQueryMatch(description) || isQueryMatch(title)
+  ));
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +30,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
