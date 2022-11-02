4ca63696500d4a3def607da6bd3dd86d52012a96
Yelisieiev Kim <rbv271@outlook.com>
2022-11-02 09:14:52 +0200

    created function normalized

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,13 @@ import moviesFromServer from './api/movies.json';
 export const App: React.FC = () => {
   const [query, setQuery] = useState('');
 
+  const normalized = (text: string) => (text.toLowerCase());
+
   const visibleMovies = moviesFromServer.filter(
-    movie => movie.title.toLowerCase().includes(query.toLowerCase())
-    || movie.description.toLowerCase().includes(query.toLowerCase()),
+    ({ title, description }) => (
+      normalized(title).includes(normalized(query))
+    || normalized(description).includes(normalized(query))
+    ),
   );
 
   return (
