0ec85a38b9f0bae5df6dc1b37902b69b68bb2882
Maksym Storozhuk <hitthemax111@gmail.com>
2022-11-09 22:02:12 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,63 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App: React.FC = () => {
-  return (
-    <div className="page">
-      <div className="page-content">
-        <div className="box">
-          <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="search-query" className="label">
-              Search movie
-            </label>
+type State = {
+  query: string
+};
 
-            <div className="control">
-              <input
-                type="text"
-                id="search-query"
-                className="input"
-                placeholder="Type search word"
-              />
+export class App extends React.Component<{}, State> {
+  state = {
+    query: '',
+  };
+
+  render() {
+    const visibleMovies = moviesFromServer
+      .filter(movie => {
+        const movieTitleCheck = movie.title.toLowerCase()
+          .includes(this.state.query.toLowerCase());
+        const movieDescriptionCheck = movie.description.toLowerCase()
+          .includes(this.state.query.toLowerCase());
+
+        return movieTitleCheck || movieDescriptionCheck;
+      });
+
+    return (
+      <div className="page">
+        <div className="page-content">
+          <div className="box">
+            <div className="field">
+              <label
+                htmlFor="search-query"
+                className="label"
+                aria-label="Search movie"
+              >
+                Search movie
+              </label>
+
+              <div className="control">
+                <input
+                  type="text"
+                  id="search-query"
+                  className="input"
+                  placeholder="Type search word"
+                  value={this.state.query}
+                  onChange={(event) => {
+                    this.setState({
+                      query: event.target.value,
+                    });
+                  }}
+                />
+              </div>
             </div>
           </div>
+
+          <MoviesList movies={visibleMovies} />
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <div className="sidebar">
+          Sidebar goes here
+        </div>
       </div>
-
-      <div className="sidebar">
-        Sidebar goes here
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
