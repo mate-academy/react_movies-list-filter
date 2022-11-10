38ba154370bd77545b39d2dba34fab0caa0c004b
Maksym Storozhuk <hitthemax111@gmail.com>
2022-11-10 19:37:55 +0200

    Requsted changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-type State = {
-  query: string
-};
+export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
 
-export class App extends React.Component<{}, State> {
-  state = {
-    query: '',
+  const movieCheck = (movie: Movie) => {
+    const movieTitleCheck = movie.title.toLowerCase()
+      .includes(query.toLowerCase());
+    const movieDescriptionCheck = movie.description.toLowerCase()
+      .includes(query.toLowerCase());
+
+    return movieTitleCheck || movieDescriptionCheck;
   };
 
-  render() {
-    const visibleMovies = moviesFromServer
-      .filter(movie => {
-        const movieTitleCheck = movie.title.toLowerCase()
-          .includes(this.state.query.toLowerCase());
-        const movieDescriptionCheck = movie.description.toLowerCase()
-          .includes(this.state.query.toLowerCase());
+  const visibleMovies = moviesFromServer
+    .filter(movie => movieCheck(movie));
 
-        return movieTitleCheck || movieDescriptionCheck;
-      });
+  const queryHandler = (event: React.ChangeEvent<HTMLInputElement>) => setQuery(
+    event.target.value,
+  );
 
-    return (
-      <div className="page">
-        <div className="page-content">
-          <div className="box">
-            <div className="field">
-              <label
-                htmlFor="search-query"
-                className="label"
-                aria-label="Search movie"
-              >
-                Search movie
-              </label>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            <label
+              htmlFor="search-query"
+              className="label"
+              aria-label="Search movie"
+            >
+              Search movie
+            </label>
 
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                  value={this.state.query}
-                  onChange={(event) => {
-                    this.setState({
-                      query: event.target.value,
-                    });
-                  }}
-                />
-              </div>
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={query}
+                onChange={queryHandler}
+              />
             </div>
           </div>
-
-          <MoviesList movies={visibleMovies} />
         </div>
 
-        <div className="sidebar">
-          Sidebar goes here
-        </div>
+        <MoviesList movies={visibleMovies} />
       </div>
-    );
-  }
-}
+
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
+    </div>
+  );
+};
