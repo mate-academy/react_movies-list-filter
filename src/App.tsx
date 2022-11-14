b8fad7dd6b2f85f41fe2e3e6eff5aa69e021ff86
Denys Topchyi <miliarder97@gmail.com>
2022-11-14 16:25:30 +0200

    react_movies-list-filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+interface Movie {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+}
+
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  function movieFilter(listOfMovies: Movie[]) {
+    return listOfMovies.filter((movie: Movie) => {
+      const condition = `${query}`;
+      const regex = new RegExp(condition, 'gi');
+
+      return movie.title.match(regex) || movie.description.match(regex);
+    });
+  }
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -20,12 +38,16 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(event) => {
+                  setQuery(event.target.value);
+                }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movieFilter(moviesFromServer)} />
       </div>
 
       <div className="sidebar">
