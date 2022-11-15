f6dcdd7b01ee36858aba41076d5ce66abe18bf94
Nhlanhla Thubana <nm.thubana@gmail.com>
2022-11-15 16:57:10 +0200

    React movie list filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+type Movie = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+};
+
+function filterMovies(movies: Movie[], query: string) {
+  const visibleMovies = movies.filter(movie => {
+    const titleExists = movie.title.toLowerCase().includes(query);
+    const descriptionExists = movie.description.toLowerCase().includes(query);
+
+    return titleExists || descriptionExists;
+  });
+
+  return visibleMovies;
+}
+
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleMovies = filterMovies(moviesFromServer, query);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +43,16 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(event) => {
+                  setQuery(event.target.value.toLowerCase());
+                }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
