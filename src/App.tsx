d8395ff2a174e26c1e20bee49919fb288b80c52d
Boris Kerekesha <kerekesha90@ukr.net>
2022-11-18 11:56:54 +0200

    movies list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [search, setSearch] = useState('');
+  const [visibleMovies, setVisibleMovies] = useState(moviesFromServer);
+  const filterMovies = () => {
+    const foundMovies = moviesFromServer.filter((movie) => {
+      const isContain = (name: keyof Movie) => movie[name]
+        .toLowerCase().includes(search.toLowerCase());
+
+      return isContain('title') || isContain('description');
+    });
+
+    return foundMovies;
+  };
+
+  useEffect(() => {
+    setVisibleMovies(filterMovies());
+  }, [search]);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +37,16 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={search}
+                onChange={(event) => {
+                  setSearch(event.target.value);
+                }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
