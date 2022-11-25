d8c15862c801ab0c82d7e1d0d3e3cc241e49c599
Valentyn Ivanenko <Ivanenko1402@gmail.com>
2022-11-25 17:15:24 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [text, setText] = useState('');
+
+  const correctList = moviesFromServer.filter(movie => {
+    const lowerText = text.toLowerCase();
+
+    return (
+      movie.title.toLowerCase().includes(lowerText)
+      || movie.description.toLowerCase().includes(lowerText)
+    );
+  });
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -20,12 +30,15 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={(event) => {
+                  setText(event.target.value);
+                }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={correctList} />
       </div>
 
       <div className="sidebar">
