cc3a0f97daec4e959771e7182edf8b1c66add4b4
Svitlana Yudina <osbka31@gmail.com>
2022-11-28 15:06:14 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
+
+export const findVisibleMovies = (movies: Movie[], query: string): Movie[] => {
+  return movies.filter(movie => {
+    const { description, title } = movie;
+
+    return description.toLocaleLowerCase()
+      .includes(query.toLocaleLowerCase())
+    || title.toLocaleLowerCase()
+      .includes(query.toLocaleLowerCase());
+  });
+};
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleMovies = findVisibleMovies(moviesFromServer, query);
+
+  const hendleChangeQuery = ((event: React.ChangeEvent<HTMLInputElement>) => (
+    setQuery(event.target.value)));
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +39,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={hendleChangeQuery}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
