6f233bef5f288e02bf19cb26fa16b5d41c490f5c
Bohdan Stepanets <number1xy@gmail.com>
2022-11-29 18:56:20 +0200

    Make movies list filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+const checkIfIncludes = (string: string, query: string): boolean => {
+  return string.toLowerCase().includes(query.toLowerCase());
+};
+
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = moviesFromServer.filter(
+    movie => checkIfIncludes(movie.title, query)
+    || checkIfIncludes(movie.description, query),
+  );
+  // (movie.title.toLowerCase()).includes(query.toLowerCase()
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +31,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
