3b30e6085793b750662eb93f5fc95e1ec4963b38
Yevhen Pavlovskyi <pavlovskiy07@gmail.com>
2022-12-06 15:57:20 +0200

    all done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+export function getVisibleMovies(
+  movies: Movie[],
+  query: string,
+) {
+  const normalQuery = query.toLowerCase();
+
+  // eslint-disable-next-line max-len
+  return movies.filter(movie => movie.title.toLowerCase().includes(normalQuery) || movie.description.toLowerCase().includes(normalQuery));
+}
+
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = getVisibleMovies(moviesFromServer, query);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +33,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={((event) => setQuery(event.target.value))}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
