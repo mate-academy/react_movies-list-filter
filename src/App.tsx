c3204352f6eb9249d453c4311b0f24ae3926aa7d
viktor-kotik <vikuso1996@gmail.com>
2022-12-07 17:50:42 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
   const [query, setFilterMovies] = useState('');
-  const visibleMovies = [...moviesFromServer.filter(
-    movie => movie.title.toLowerCase().includes(query.toLowerCase())
-      || movie.description.toLowerCase().includes(query.toLowerCase()),
-  )];
+  const visibleMovies = moviesFromServer.filter(movie => (
+    movie.title.toLowerCase().includes(query.toLowerCase())
+    || movie.description.toLowerCase().includes(query.toLowerCase())
+  ));
 
   return (
     <div className="page">
