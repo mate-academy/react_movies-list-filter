65445b01b2f5ed17081d95136e970b9c54f28914
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2023-01-02 19:09:55 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  // const debounce = (fn: () => {}, ms: number) => {
+  //   let timeout: any;
+  //
+  //   return function ():void {
+  //     const fnCall = () => {
+  //       fn.apply(this, arguments);
+  //     };
+  //
+  //     clearTimeout(timeout);
+  //     timeout = setTimeout(fnCall, ms);
+  //   };
+  // };
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setQuery(event.currentTarget.value);
+  }
+
+  // const handleChanged = debounce(handleChange, 500);
+
+  const visibleMovies = moviesFromServer.filter(movie => {
+    const lowerCaseInput = query.trim().toLowerCase();
+    const lowerMovieTitle = movie.title.toLowerCase();
+    const lowerMovieDescription = movie.description.toLowerCase();
+
+    return (
+      lowerMovieTitle.includes(lowerCaseInput)
+      || lowerMovieDescription.includes(lowerCaseInput)
+    );
+  });
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +52,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={handleChange}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
