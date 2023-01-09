ee4890bc83e067a83cc962401cfc59640127e8ee
Roman Korchovskyi <korchovskyi.roman@gmail.com>
2023-01-10 00:49:07 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setSearchValue] = useState('');
+  const [visibleMovies, setVisibleMovies] = useState(moviesFromServer);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+
+    setSearchValue(inputValue);
+
+    const filteredMovies = moviesFromServer.filter((movie) => {
+      return (
+        movie.title.toLowerCase()
+          .split(' ').join('')
+          .includes(inputValue.toLowerCase().split(' ').join(''))
+        || movie.description.toLowerCase()
+          .split(' ').join('')
+          .includes(inputValue.toLowerCase().split(' ').join(''))
+      );
+    });
+
+    setVisibleMovies(filteredMovies);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
@@ -13,24 +35,21 @@ export const App: React.FC = () => {
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
-
             <div className="control">
               <input
                 type="text"
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={handleChange}
               />
             </div>
           </div>
         </div>
-
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
-
-      <div className="sidebar">
-        Sidebar goes here
-      </div>
+      <div className="sidebar">Sidebar goes here</div>
     </div>
   );
 };
