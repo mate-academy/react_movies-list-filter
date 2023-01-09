86160e72394256554b4566e45d00c12f10e57095
Vladimir Arkanov <vladimirarkanov@icloud.com>
2023-01-09 22:50:24 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setProperQuery] = useState('');
+
+  const changeValue = (event:React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setProperQuery(value);
+  };
+
+  const visibleMovies = moviesFromServer.filter(movie => {
+    const title = movie.title.toLowerCase();
+    const description = movie.description.toLowerCase();
+    const properQuery = query.toLowerCase().trim();
+
+    return title.includes(properQuery) || description.includes(properQuery);
+  });
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +36,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={changeValue}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
