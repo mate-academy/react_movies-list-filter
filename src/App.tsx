f52269555589c52758e817df485fc9684d4f1f97
Ivan Melnychuk <jacksmilereacher@gmail.com>
2023-01-09 23:30:43 +0200

    added useState hook and search logic to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const formattedSearchQuery = searchQuery.toLocaleLowerCase().trim();
+  const filteredMovies
+    = moviesFromServer.filter(movie =>
+      // eslint-disable-next-line implicit-arrow-linebreak
+      movie.title.toLocaleLowerCase()
+        .includes(formattedSearchQuery)
+      || movie.description.toLocaleLowerCase()
+        .includes(formattedSearchQuery));
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +31,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={searchQuery}
+                onChange={(event) => setSearchQuery(event.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={filteredMovies} />
       </div>
 
       <div className="sidebar">
