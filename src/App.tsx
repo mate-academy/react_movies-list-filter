90a12294279ce53d38ce588de287403bffd866d6
Mykhailyk-Vadym <ihappyspam@gmail.com>
2023-01-09 15:09:33 +0200

    movies-list-filter created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [searchMovie, setSearchMovie] = useState('');
+
+  const visibleMovies = moviesFromServer.filter((movie) => (
+    movie.title.toLowerCase().includes(
+      searchMovie.toLowerCase().trim(),
+    )
+    || movie.description.toLowerCase().includes(
+      searchMovie.toLowerCase().trim(),
+    )
+  ));
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +31,16 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={searchMovie}
+                onChange={(event) => {
+                  setSearchMovie(event.target.value);
+                }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
