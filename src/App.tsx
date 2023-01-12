c3338523c090fc0b0ff7291d7cf8fa85f64c102a
JRijo <99342891+John-Stiff@users.noreply.github.com>
2023-01-12 17:11:26 +0000

    created useState hook for form onChange event

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import { FC, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App: React.FC = () => {
+export const App: FC = () => {
+  const [filterValue, setFilterValue] = useState('');
+
+  const visibleMovies = moviesFromServer.filter(({
+    title,
+    description,
+  }) => {
+    const filterArgs = [
+      `${title} ${description}`,
+      filterValue.trim(),
+    ]
+      .map(string => string.toLowerCase());
+
+    const [titleAndDescription, query] = filterArgs;
+
+    return titleAndDescription.includes(query);
+  });
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +37,16 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={filterValue}
+                onChange={(filterEvent) => {
+                  setFilterValue(filterEvent.target.value);
+                }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
