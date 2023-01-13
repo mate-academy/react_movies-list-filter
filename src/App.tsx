6b8fa39e6640cf4bd21adde0c164cc94797cc267
JzSol <111354281+JzSol@users.noreply.github.com>
2023-01-13 15:03:20 +0200

    Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState<string>('');
+
+  const cleanQuery = query.trim().toLowerCase();
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const visibleMovies = moviesFromServer.filter((movie) => {
+    const loverTitle = movie.title.toLowerCase();
+    const loverDescription = movie.description.toLowerCase();
+
+    return (
+      loverTitle.includes(cleanQuery) || loverDescription.includes(cleanQuery)
+    );
+  });
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,17 +37,17 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={handleQueryChange}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <div className="sidebar">
-        Sidebar goes here
-      </div>
+      <div className="sidebar">Sidebar goes here</div>
     </div>
   );
 };
