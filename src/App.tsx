430647d16d53d98d000b56978c33830a44044d5d
Vasya Rymarenko <vrima60@gmail.com>
2023-01-16 18:16:48 +0200

    fixed id name of search-query

--- src/App.tsx
+++ src/App.tsx
@@ -24,17 +24,17 @@ export const App: React.FC = () => {
         <div className="box">
           <div className="field">
             {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="search-searchQuery" className="label">
+            <label htmlFor="search-query" className="label">
               Search movie
             </label>
 
             <div className="control">
               <input
                 type="text"
-                id="search-searchQuery"
+                id="search-query"
                 className="input"
                 placeholder="Type search word"
-                name="searchQuery"
+                name="query"
                 value={searchQuery}
                 onChange={(event) => {
                   setSearchQuery(event.target.value);
