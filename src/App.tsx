0504aa105dc5477a56d7f96bc422669e899a00b3
Anna Inozemets <inoznetta@gmail.com>
2023-01-30 21:45:15 +0200

    Movie list search input was realised

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
   return (
     <div className="page">
       <div className="page-content">
@@ -17,15 +19,20 @@ export const App: React.FC = () => {
             <div className="control">
               <input
                 type="text"
+                name="query"
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(event) => {
+                  setQuery(event.target.value);
+                }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={moviesFromServer} filterItem={query} />
       </div>
 
       <div className="sidebar">
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -4,12 +4,27 @@ import { MovieCard } from '../MovieCard';
 
 interface Props {
   movies: Movie[];
+  filterItem: string;
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList: React.FC<Props> = ({ movies, filterItem }) => {
+  const filterItemLowerCase = filterItem.toLowerCase().trim();
+  const visibleMovies = movies
+    .filter(({ title, description }) => {
+      const titleLowerCase = title.toLowerCase();
+      const descriptionlowerCase = description.toLowerCase();
+
+      const inTitle = titleLowerCase.includes(filterItemLowerCase);
+      const inDescription = descriptionlowerCase.includes(filterItemLowerCase);
+
+      return inTitle || inDescription;
+    });
+
+  return (
+    <div className="movies">
+      {visibleMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
