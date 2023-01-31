14cf27aea6e54937fa46f77f5befa2f6b1111391
Mykyta Krainik <nikinta2003@gmail.com>
2023-01-31 11:23:24 +0200

    Implement search logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleMovies = moviesFromServer.filter(movie => {
+    const movieTitleLowerCase = movie.title.toLowerCase();
+    const movieDescLowerCase = movie.description.toLowerCase();
+    const queryLowerCase = query.trim().toLowerCase();
+
+    return movieTitleLowerCase.includes(queryLowerCase)
+      || movieDescLowerCase.includes(queryLowerCase);
+  });
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -17,15 +27,18 @@ export const App: React.FC = () => {
             <div className="control">
               <input
                 type="text"
+                name="searchQuery"
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={event => setQuery(event.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
