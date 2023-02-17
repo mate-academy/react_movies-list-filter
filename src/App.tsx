dd54aade71b9441db7abe50de5d14a5582578bd6
Sasha Semenyuk <gud.semenyuk@gmail.com>
2023-02-17 19:12:04 +0200

    Added change after reject

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,24 @@ import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
-// import { values } from 'cypress/types/lodash';
 
 export const App: React.FC = () => {
-  const [searchMovieName, setSearchMovieName] = useState('');
-  const correstMovieName = searchMovieName.toLowerCase().trim();
-  const filteredMovies = moviesFromServer
-    .filter(film => film.title.toLowerCase().includes(correstMovieName)
-    || film.description.toLowerCase().includes(correstMovieName));
+  const [query, setQuery] = useState('');
+  const correstMovieName = query.toLowerCase().trim();
+
+  const filteredMovies = moviesFromServer.filter(
+    film => {
+      const filmInfo = (film.title + film.description).toLowerCase();
+
+      return filmInfo.includes(correstMovieName);
+    },
+  );
 
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -27,15 +30,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
-                value={searchMovieName}
-                onChange={(event) => setSearchMovieName(event.target.value)}
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
               />
             </div>
           </div>
         </div>
 
         <MoviesList movies={filteredMovies} />
-        {/* <MoviesList movies={moviesFromServer} /> */}
       </div>
 
       <div className="sidebar">
