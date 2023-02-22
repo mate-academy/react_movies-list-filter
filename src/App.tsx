c5d74b58314c77cf5da8c5944d6797ae11971e4d
Vasiliy22 <hitretsukrnet@gmail.com>
2023-02-22 22:54:59 +0200

    Solves

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [nameMovies, setSearch] = useState('');
+
+  const visibleMovies = moviesFromServer.filter(list => (
+    list.title.toLowerCase().includes(nameMovies)
+    || list.description.toLowerCase().includes(nameMovies)
+  ));
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +27,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={nameMovies.toLowerCase()}
+                onChange={(e) => setSearch(e.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
@@ -34,3 +43,24 @@ export const App: React.FC = () => {
     </div>
   );
 };
+
+//  <div className="page-content">
+//   <div className="box">
+//     <div className="field">
+//            <label htmlFor="search-query" className="label">
+//              Search movie
+//          </label>
+
+//      <div className="control">
+//              <input
+//               type="text"
+//                 id="search-query"
+//                 className="input"
+//                  placeholder="Type search word"
+//                />
+//              </div>
+//            </div>
+//          </div>
+
+//  <MoviesList movies={moviesFromServer} />
+//   </div>
