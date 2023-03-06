604868bb6760fad869afa418692aa6989cef8c23
Nick Busarhin <busarginkolia@gmail.com>
2023-03-06 14:43:09 +0200

    movie search

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+type Props = {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+};
+
+function filterMovies(query: string, films: Props[]) {
+  const lowCaseQuery = query.toLowerCase();
+  const visibleMovies
+    = films.filter((movie) => movie.title.toLowerCase().includes(lowCaseQuery)
+    || movie.description.toLowerCase().includes(lowCaseQuery));
+
+  return visibleMovies;
+}
+
 export const App: React.FC = () => {
+  const [query, setNewQuery] = useState('');
+
+  const visibleMovies = filterMovies(query, moviesFromServer);
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -20,12 +40,13 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={(event) => setNewQuery(event.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
