41ed47e371797aa8ed35a2da04e45328b0f1f437
Kyrychenko Illia <i.kirichenko160804@gmail.com>
2023-03-08 00:13:53 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+type MovieSearchFilters = {
+  title: string,
+  description: string
+};
+
+const formatString = (string: string): string => {
+  return string.toLocaleLowerCase().trim();
+};
+
+const checkMovie = ({
+  title,
+  description,
+}: MovieSearchFilters,
+query: string) => {
+  const formattedQuery = formatString(query);
+  const formattedTitle = formatString(title);
+  const formattedDescription = formatString(description);
+  const isTitleContainsQuery = formattedTitle.includes(formattedQuery);
+  const isDescriptionContainsQuery
+    = formattedDescription.includes(formattedQuery);
+
+  return isTitleContainsQuery || isDescriptionContainsQuery;
+};
+
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = moviesFromServer
+    .filter(movie => checkMovie(movie, query));
+
+  const handleCheck = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+
+    setQuery(inputValue);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +54,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={handleCheck}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
