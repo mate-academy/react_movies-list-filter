fce0e4a157f1c875a88f6bef7fea1934e678fb03
Rashyd <rashydlezgi@gmail.com>
2023-03-19 14:32:36 +0200

    movies list filter was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleMovies = moviesFromServer.filter(({ title, description }) => {
+    const titleFormatted = title.toLowerCase();
+    const descriptionFormatted = description.toLowerCase();
+    const queryFormatted = query.trim().toLowerCase();
+
+    const foundInTitle = titleFormatted.includes(queryFormatted);
+    const foundInDescription = descriptionFormatted.includes(queryFormatted);
+
+    return foundInTitle || foundInDescription;
+  });
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -20,12 +32,15 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={(event) => {
+                  setQuery(event.target.value);
+                }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
