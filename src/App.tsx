1c127f7173c394b8fd7a5b2dda5fcf8d75d51dec
gretoq <dovhun3d@gmail.com>
2023-03-21 23:08:18 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -17,15 +22,21 @@ export const App: React.FC = () => {
             <div className="control">
               <input
                 type="text"
-                id="search-query"
                 className="input"
+                name="query"
+                value={query}
+                onChange={handleQueryChange}
+                id="search-query"
                 placeholder="Type search word"
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList
+          movies={moviesFromServer}
+          query={query}
+        />
       </div>
 
       <div className="sidebar">
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { memo } from 'react';
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
 interface Props {
   movies: Movie[];
+  query: string;
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const filterMovies = (
+  movies: Movie[],
+  query: string,
+): Movie[] => {
+  if (!query) {
+    return movies;
+  }
+
+  const normalizedQuery = query.toLowerCase().trim();
+
+  return movies
+    .filter(movie => {
+      const { title, description } = movie;
+      const normalizedTitle = title.toLowerCase();
+      const normalizedDescription = description.toLowerCase();
+
+      return (normalizedTitle.includes(normalizedQuery)
+        || normalizedDescription.includes(normalizedQuery)
+      );
+    });
+};
+
+export const MoviesList: React.FC<Props> = memo(({ movies, query }) => {
+  const visibleMovies = filterMovies(movies, query);
+
+  return (
+    <div className="movies">
+      {visibleMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+});
