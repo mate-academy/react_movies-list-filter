b11f90f8f2f6f5ff21a188690fded17aa8cee66c
Yevhenii Leichenko <eugeniy0536@gmail.com>
2023-03-22 17:11:08 +0200

    React movies list filter - Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const handleChangeQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const filterMovies = (movieList: Movie[]) => {
+    const searchValue = query.toLowerCase().trim();
+
+    return [...movieList].filter((movie) => {
+      return movie.title.toLocaleLowerCase().includes(searchValue)
+        || movie.description.toLocaleLowerCase().includes(searchValue);
+    });
+  };
+
+  const filteredListOfMovies = filterMovies(moviesFromServer);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,17 +37,17 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={handleChangeQuery}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={filteredListOfMovies} />
       </div>
 
-      <div className="sidebar">
-        Sidebar goes here
-      </div>
+      <div className="sidebar">Sidebar goes here</div>
     </div>
   );
 };
