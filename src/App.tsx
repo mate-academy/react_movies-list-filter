0b350dc549fe141d8c4a0cfb3c92640c204365e0
Dovhun Artem <dovhun3d@gmail.com>
2023-03-23 15:09:27 +0200

    Replaced filtere logic to the parent component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,38 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+export const isMatchQuery = (text: string, query: string): boolean => {
+  const normalizedText = text.toLowerCase();
+  const normalizedQuery = query.toLowerCase().trim();
+
+  return normalizedText.includes(normalizedQuery);
+};
+
+export const filterMovies = (
+  movies: Movie[],
+  query: string,
+): Movie[] => {
+  if (!query) {
+    return movies;
+  }
+
+  return movies
+    .filter(({ title, description }) => {
+      const foundInTitle = isMatchQuery(title, query);
+      const foundInDescription = isMatchQuery(description, query);
+
+      return foundInTitle || foundInDescription;
+    });
+};
+
 export const App: React.FC = () => {
   const [query, setQuery] = useState('');
 
   const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
   };
+
+  const visibleMovies = filterMovies(moviesFromServer, query);
 
   return (
     <div className="page">
@@ -34,8 +60,8 @@ export const App: React.FC = () => {
         </div>
 
         <MoviesList
-          movies={moviesFromServer}
-          query={query}
+          movies={visibleMovies}
+          // query={query}
         />
       </div>
 
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -4,41 +4,12 @@ import { MovieCard } from '../MovieCard';
 
 interface Props {
   movies: Movie[];
-  query: string;
 }
 
-const isMatchQuery = (text: string, query: string): boolean => {
-  const normalizedText = text.toLowerCase();
-  const normalizedQuery = query.toLowerCase().trim();
-
-  return normalizedText.includes(normalizedQuery);
-};
-
-export const filterMovies = (
-  movies: Movie[],
-  query: string,
-): Movie[] => {
-  if (!query) {
-    return movies;
-  }
-
-  return movies
-    .filter(({ title, description }) => {
-      const foundInTitle = isMatchQuery(title, query);
-      const foundInDescription = isMatchQuery(description, query);
-
-      return foundInTitle || foundInDescription;
-    });
-};
-
-export const MoviesList: React.FC<Props> = memo(({ movies, query }) => {
-  const visibleMovies = filterMovies(movies, query);
-
-  return (
-    <div className="movies">
-      {visibleMovies.map(movie => (
-        <MovieCard key={movie.imdbId} movie={movie} />
-      ))}
-    </div>
-  );
-});
+export const MoviesList: React.FC<Props> = memo(({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard key={movie.imdbId} movie={movie} />
+    ))}
+  </div>
+));
