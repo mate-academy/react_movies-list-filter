4a6095fe5598ba2fd6d631a0cca361f0ea98be66
Vadym Tsyndra <Mr.Tsyndra@gmail.com>
2023-03-23 20:56:32 +0200

    Solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -4,11 +4,14 @@ import { MovieCard } from '../MovieCard';
 
 interface Props {
   movies: Movie[];
+  visibleMovies: Movie[];
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
+export const MoviesList: React.FC<Props> = ({ movies, visibleMovies }) => (
   <div className="movies">
-    {movies.map(movie => (
+    {visibleMovies ? visibleMovies.map(movie => (
+      <MovieCard key={movie.imdbId} movie={movie} />
+    )) : movies.map(movie => (
       <MovieCard key={movie.imdbId} movie={movie} />
     ))}
   </div>
