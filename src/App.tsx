633e127915eb19dec700e086c3567a1ccffe08b6
Maksym <maksymmaliuk98@gmail.com>
2023-03-24 16:26:47 +0200

    Implemented movie filtering functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+const isValidString = (value: string, query: string) => {
+  return value.toLowerCase().includes(query.toLowerCase().trim());
+};
+
+const filteredMovies = (query: string) => {
+  return moviesFromServer.filter(movie => {
+    const { title, description } = movie;
+
+    return isValidString(title, query) || isValidString(description, query);
+  });
+};
+
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const visibleMovies = filteredMovies(query);
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -20,12 +39,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={handleChange}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
