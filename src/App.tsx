925ff2152667d8f798924f6240faea11b5e23833
Ashton <nonlocality@gmail.com>
2023-03-25 14:07:00 +0200

    Solve

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import {
+  FC, useState, useMemo, useCallback,
+} from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App: React.FC = () => {
+const inMovieText = (value: string, query: string) => value.toLowerCase()
+  .includes(query.trim().toLowerCase());
+
+export const App: FC = () => {
+  const [query, setQuery] = useState('');
+  const matchedMovies = useMemo(() => {
+    return moviesFromServer.filter(({ title, description }) => {
+      return inMovieText(title.concat(description), query);
+    });
+  }, [moviesFromServer, query]);
+  const handleChange = useCallback(({ target: { value } }) => {
+    setQuery(value);
+  }, []);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -13,21 +28,20 @@ export const App: React.FC = () => {
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
-
             <div className="control">
               <input
                 type="text"
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={handleChange}
               />
             </div>
           </div>
         </div>
-
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={matchedMovies} />
       </div>
-
       <div className="sidebar">
         Sidebar goes here
       </div>
