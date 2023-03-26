d9837471215d10ca7cd2a705079bbfad0ba74d9b
andrej <andersontwentytwo@gmail.com>
2023-03-26 15:59:22 +0300

    React movies filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const searchQueryIn = (field: string) => (
+    field.toLowerCase().includes(query.toLowerCase().trim())
+  );
+
+  const visibleMovies = moviesFromServer.filter(
+    ({ title, description }) => (
+      searchQueryIn(title) || searchQueryIn(description)
+    ),
+  );
+
   return (
-    <div className="page">
+    <div className="page s s">
       <div className="page-content">
         <div className="box">
-          <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="search-query" className="label">
+          <div className="field ">
+            <label
+              htmlFor="search-query"
+              className="label"
+            >
               Search movie
             </label>
 
@@ -19,17 +37,19 @@ export const App: React.FC = () => {
                 type="text"
                 id="search-query"
                 className="input"
-                placeholder="Type search word"
+                placeholder="Search"
+                value={query}
+                onChange={handleQueryChange}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
-      </div>
-
-      <div className="sidebar">
-        Sidebar goes here
+        <div className="movies__container">
+          <MoviesList
+            movies={visibleMovies}
+          />
+        </div>
       </div>
     </div>
   );
