5d52b1112317400dc0f3ba289c3d7baa78374805
vadosik15 <vadim.bratus@gmail.com>
2023-03-26 18:31:00 +0300

    created the program

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+/* eslint-disable no-sequences */
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+function filteredMovies(moviesServer: Movie[], mainstr: string): Movie[] {
+  const visibleMovies: Movie[] = moviesServer.filter((film: Movie) => film
+    .title.toLowerCase().includes(mainstr.toLowerCase().trim())
+    || film.description.toLowerCase().includes(mainstr.toLowerCase().trim()));
+
+  return visibleMovies;
+}
+
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleMovies: Movie[] = filteredMovies(moviesFromServer, query);
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -20,12 +32,16 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(event) => (
+                  setQuery(event.target.value)
+                )}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
