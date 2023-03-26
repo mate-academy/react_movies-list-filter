b98aa11f1c39fb6504785ecd87b4e26679f6d841
vadym4che <vadym4che@gmail.com>
2023-03-26 15:47:45 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const [moviesToShow, setMoviesToShow] = useState(moviesFromServer);
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setQuery(value);
+  };
+
+  const filterMovies = () => {
+    const filtered = moviesFromServer.filter(({ title, description }) => {
+      const lowerCasedTitle = title.toLowerCase();
+      const lowerCasedDescription = description.toLowerCase();
+      const lowerCasedQuery = query.toLowerCase().trim();
+
+      return lowerCasedTitle.includes(lowerCasedQuery)
+        || lowerCasedDescription.includes(lowerCasedQuery);
+    });
+
+    setMoviesToShow(filtered);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      filterMovies();
+    }, 500);
+  }, [query]);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +48,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(event) => handleSearch(event)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={moviesToShow} />
       </div>
 
       <div className="sidebar">
