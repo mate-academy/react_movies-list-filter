ccf92d64ddc2a6fc40fc9ec06df581771836d443
GUSILLUS <zebela2002gmail.com>
2023-04-05 17:15:30 +0300

    Solutions

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+const normalize = (value: string) => value.toLowerCase();
+
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleMovies = moviesFromServer.filter(movie => (
+    normalize(movie.title).includes(normalize(query))
+    || normalize(movie.description).includes(normalize(query))
+  ));
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="search-query" className="label">
+            <label
+              htmlFor="search-query"
+              className="label"
+            >
               Search movie
             </label>
 
@@ -20,12 +31,18 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(e) => {
+                  const { value } = e.target;
+
+                  setQuery(value.trim());
+                }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
