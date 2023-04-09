0a797c41d6ddc76138983f9eba32e16f243be1db
Dmytro Zhuk <dave.beetle2311@gmail.com>
2023-04-09 18:14:54 +0300

    Ready

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import moviesFromServer from './api/movies.json';
 export const App: React.FC = () => {
   const [query, setQuery] = useState('');
 
-  const visibleMovies = [...moviesFromServer].filter((movie) => {
+  const visibleMovies = moviesFromServer.filter((movie) => {
     const title = movie.title.toLowerCase();
     const description = movie.description.toLowerCase();
     const queryLowerCase = query.toLowerCase().trim();
