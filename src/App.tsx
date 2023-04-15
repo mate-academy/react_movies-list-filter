1c088b76d6261e989762f99e87b8aebadaab88a2
Vadym Dubniak <dubnyakv@gmail.com>
2023-04-15 18:43:30 +0300

    implemented filtering for movie list by using search query

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, changeQuery] = useState('');
+  const visibleMovies = moviesFromServer
+    .filter(movie => (
+      movie.description.toLowerCase().includes(query.toLowerCase())
+      || movie.title.toLowerCase().includes(query.toLowerCase())
+    ));
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    changeQuery(event.target.value.trim());
+  };
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +31,13 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={handleQueryChange}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
