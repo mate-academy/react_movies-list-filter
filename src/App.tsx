65f7244f9f0e17a434d03f331975c4b62572eb08
Sergey Mikhalskiy <msa1991work@gmail.com>
2023-04-16 15:51:29 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const handleChangeQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setQuery(value);
+  };
+
+  const getLowerString = (str: string): string => {
+    return str.toLowerCase();
+  };
+
+  const handleSearchFilms = (films: Movie[]) => {
+    const lowerQuery = getLowerString(query).trim();
+
+    return films.filter(({ title, description }: Movie) => {
+      const lowerTitle = getLowerString(title);
+      const lowerDescription = getLowerString(description);
+
+      return lowerTitle.includes(lowerQuery)
+        || lowerDescription.includes(lowerQuery);
+    });
+  };
+
+  const visibleMovies = handleSearchFilms(moviesFromServer);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +46,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={handleChangeQuery}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
