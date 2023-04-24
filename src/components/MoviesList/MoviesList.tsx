6a6ad757f13b27e787e2348ec82a793fd17608a2
Alex Rog <sasharg303@gmail.com>
2023-04-24 13:39:51 +0200

    WorkedVersion

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -4,12 +4,18 @@ import { MovieCard } from '../MovieCard';
 
 interface Props {
   movies: Movie[];
+  query: string;
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
+export const MoviesList: React.FC<Props> = ({ movies, query }) => (
   <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
+    {movies.map(movie => {
+      const isNeededTitle = movie.title.toLowerCase().includes(query.toLowerCase().trim()); // eslint-disable-line
+      const isNeededDesc = movie.description.toLowerCase().includes(query.toLowerCase().trim()); // eslint-disable-line
+
+      return (isNeededTitle || isNeededDesc) && (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      );
+    })}
   </div>
 );
