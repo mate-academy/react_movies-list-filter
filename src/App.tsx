7db5c218a8773d9a7c375cd3b9a0b7c404f43dee
Andrii <remote.makandriival@gmail.com>
2023-04-24 13:47:57 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setQuery(event.currentTarget.value);
+  };
+
+  const visibleMovies = moviesFromServer
+    .filter(movie => {
+      const {
+        title,
+        description,
+      } = movie;
+
+      return title
+        .toLowerCase()
+        .includes(query.toLowerCase())
+        || description
+          .toLowerCase()
+          .includes(query.toLowerCase());
+    });
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +41,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={handleInputChange}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
