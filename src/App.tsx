8bbb0116cdc655886f4d7f6cea53567fff933aa8
Mark Boiko <boikomark555@gmail.com>
2023-04-24 21:34:27 +0300

    completed movie list filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+function isValidFilm(filmProperty: string, query: string): boolean {
+  return filmProperty.toLowerCase().includes(query.toLowerCase().trim());
+}
+
 export const App: React.FC = () => {
+  const [query, changeQuery] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement> &
+  { target : HTMLButtonElement }) => {
+    const { value } = event.target;
+
+    changeQuery(value);
+  };
+
+  const visibleMovies = moviesFromServer.filter(({ title, description }) => (
+    isValidFilm(title, query) || isValidFilm(description, query)));
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +36,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={handleChange}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
