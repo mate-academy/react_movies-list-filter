83a1f4177679caa09cf6269acead7ca71c72ee59
Alex Rog <sasharg303@gmail.com>
2023-04-24 14:16:09 +0200

    FixedReapet

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -7,11 +7,15 @@ interface Props {
   query: string;
 }
 
+function isNeeded(movieInfo: string, query: string) {
+  return movieInfo.toLowerCase().includes(query.toLowerCase().trim());
+}
+
 export const MoviesList: React.FC<Props> = ({ movies, query }) => (
   <div className="movies">
     {movies.map(movie => {
-      const isNeededTitle = movie.title.toLowerCase().includes(query.toLowerCase().trim()); // eslint-disable-line
-      const isNeededDesc = movie.description.toLowerCase().includes(query.toLowerCase().trim()); // eslint-disable-line
+      const isNeededTitle = isNeeded(movie.title, query);
+      const isNeededDesc = isNeeded(movie.description, query);
 
       return (isNeededTitle || isNeededDesc) && (
         <MovieCard key={movie.imdbId} movie={movie} />
