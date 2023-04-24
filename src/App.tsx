ef6bbbc2e02b4f30d0b59926b9c148374368ad4d
Valery Sazonov <vksazonov@gmail.com>
2023-04-24 18:20:58 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const isIncluded = (text: string) => (text.trim()
+    .toLowerCase()
+    .includes(query.trim().toLowerCase()))
+  const visibleMovies = moviesFromServer.filter(movie => (
+    isIncluded(movie.title)
+    || isIncluded(movie.description)
+  ));
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +29,13 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={event => setQuery(event.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
