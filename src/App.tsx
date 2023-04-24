6e44c87db987d6e45852cbc8d3c6195d55060553
kozlo <kozlovilla@gmail.com>
2023-04-24 16:52:55 +0300

    implement search filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [search, setSearch] = useState('');
+
+  const searchFilter = (movie: Movie) => {
+    const { title, description } = movie;
+
+    return title.toLowerCase().includes(search.toLowerCase())
+      || description.toLowerCase().includes(search.toLowerCase());
+  };
+
+  const filteredMovies = moviesFromServer.filter(searchFilter);
+
+  const handleChange = (event: {
+    target: { value: React.SetStateAction<string>; }; }) => {
+    setSearch(event.target.value);
+  };
+
+  const moviesOnScreen = search ? filteredMovies : moviesFromServer;
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -20,12 +37,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={handleChange}
+                value={search}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={moviesOnScreen} />
       </div>
 
       <div className="sidebar">
