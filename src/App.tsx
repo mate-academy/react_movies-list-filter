cf542d7da1c49117361b47e1f6e179e56e355668
Nazarii Chepil <nazzikk1998@gmail.com>
2023-04-25 18:56:39 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,32 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+function includes(str: string, substr: string): boolean {
+  return str.toLowerCase().includes(substr.toLowerCase().trim());
+}
+
 export const App: React.FC = () => {
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const [filteredMovies, setFilteredMovies] = React.useState(moviesFromServer);
+
+  const handleSearchQueryChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setSearchQuery(event.target.value);
+
+    setFilteredMovies(moviesFromServer.filter((movie) => {
+      const { title, description } = movie;
+
+      return includes(title, event.target.value)
+        || includes(description, event.target.value);
+    }));
+  };
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -20,12 +39,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={searchQuery}
+                onChange={handleSearchQueryChange}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={filteredMovies} />
       </div>
 
       <div className="sidebar">
