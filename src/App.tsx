7cfeff5b6d03a6d05d5d3e2e4de38aaabbb75baf
Hamuud <artemlask2014@gmail.com>
2023-04-25 13:05:04 +0300

    Create seatchBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleMovies = moviesFromServer.filter((movie) => {
+    const lowerMovieTitle = movie.title.toLowerCase();
+    const lowerMovieDescription = movie.description.toLowerCase();
+    const querySearch = query.toLowerCase().trim();
+
+    return (lowerMovieTitle.includes(querySearch)
+    || lowerMovieDescription.includes(querySearch));
+  });
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +31,13 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={event => setQuery(event.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
