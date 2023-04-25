d0b383cdc092294744bd31f03158176325f81c45
Tolik Petras <toliketras4@gmail.com>
2023-04-25 18:00:03 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [searchValue, setSearchValue] = useState('');
+
+  const onChange = (el: React.FormEvent<HTMLInputElement>) => {
+    setSearchValue(el.currentTarget.value);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +26,13 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={onChange}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={moviesFromServer} searchQuery={searchValue} />
       </div>
 
       <div className="sidebar">
