4e9963d3b994ac5d931933d9a08adb9e6ed2bd5d
Nikita Zadorozhnyi <nikita.zadorozhnyi@nure.ua>
2023-04-25 11:42:34 +0300

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent} from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
@@ -6,12 +6,15 @@ import moviesFromServer from './api/movies.json';
 export const App: React.FC = () => {
   const [query, setQuery] = useState('');
 
-  const visebleMovies = moviesFromServer.filter(movie => (
-    movie.title.toLowerCase().includes(query.toLowerCase().trim())
-    || movie.description.toLowerCase().includes(query.toLowerCase().trim())
+  const doesQueryMatchMovie = (movie: string) => {
+    return movie.toLowerCase().includes(query.toLowerCase().trim());
+  };
+
+  const visebleMovies = moviesFromServer.filter(({ title, description }) => (
+    doesQueryMatchMovie(title) || doesQueryMatchMovie(description)
   ));
 
-  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleQueryChange = (event: ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
   };
 
