d92b4cacb19f5e19231105f0944d4006a84c5dc3
Tanya Sydoruk <tanyasydoruk113022@gmail.com>
2023-04-28 22:03:26 +0300

    \solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const isPropertyMatchQuery = (movieProperty: string) => (
+    movieProperty
+      .trim()
+      .toLocaleLowerCase()
+      .includes(query.toLocaleLowerCase().trim())
+  );
+
+  const visibleMovies = moviesFromServer.filter(movie => (
+    isPropertyMatchQuery(movie.title)
+    || isPropertyMatchQuery(movie.description)
+  ));
+
+  const handleUserQuery = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -17,6 +34,8 @@ export const App: React.FC = () => {
             <div className="control">
               <input
                 type="text"
+                value={query}
+                onChange={handleUserQuery}
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
@@ -25,7 +44,7 @@ export const App: React.FC = () => {
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
