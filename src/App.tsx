55df2206846065f89d7036ece323799be3199b8a
Artem Kyrylenko <kyrylenko.a.a@gmail.com>
2023-04-28 17:23:01 +0300

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,19 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+function isMovieMatchQuery(filmProperty: string, query: string): boolean {
+  return filmProperty.toLowerCase().includes(query.toLowerCase().trim());
+}
+
 export const App: React.FC = () => {
   const [query, setQuery] = useState('');
+
+  const visibleMovies = moviesFromServer.filter(({ title, description }) => (
+    isMovieMatchQuery(title, query) || isMovieMatchQuery(description, query)));
+
+  const handleQuery = (event: React.ChangeEvent<HTMLInputElement>) => (
+    setQuery(event.target.value)
+  );
 
   return (
     <div className="page">
@@ -23,14 +34,12 @@ export const App: React.FC = () => {
                 className="input"
                 placeholder="Type search word"
                 value={query}
-                onChange={(event) => {
-                  setQuery(event.target.value);
-                }}
+                onChange={handleQuery}
               />
             </div>
           </div>
         </div>
-        <MoviesList movies={moviesFromServer} query={query} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -4,22 +4,12 @@ import { MovieCard } from '../MovieCard';
 
 interface Props {
   movies: Movie[];
-  query: string;
 }
 
-export const MoviesList: React.FC<Props> = ({ movies, query }) => {
-  const visibleMovies = movies.filter((movie) => {
-    const formattedTitle = movie.title.toLowerCase();
-    const formattedDescription = movie.description.toLowerCase();
-    const formattedQuery = query.toLowerCase().trim();
-
-    return formattedTitle.includes(formattedQuery)
-      || formattedDescription.includes(formattedQuery);
-  });
-
+export const MoviesList: React.FC<Props> = ({ movies }) => {
   return (
     <div className="movies">
-      {visibleMovies.map(movie => (
+      {movies.map(movie => (
         <MovieCard key={movie.imdbId} movie={movie} />
       ))}
     </div>
