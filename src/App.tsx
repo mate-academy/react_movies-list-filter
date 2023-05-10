9137436e5964ba9dfcfe4d4ac9c3aa1e070174f6
Ilia <koleshnik160799@gmail.com>
2023-05-10 15:16:45 +0300

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+// eslint-disable max-len
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  // eslint-disable-next-line
+  let [valueInput, valueFunc] = useState('');
+
+  const filterMovie = moviesFromServer.filter(movie => {
+    return movie.title.toLocaleLowerCase()
+      .indexOf(valueInput.toLocaleLowerCase()) >= 0
+    || movie.description.toLocaleLowerCase()
+      .indexOf(valueInput.toLocaleLowerCase()) >= 0;
+  });
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +31,15 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={(e) => {
+                  valueFunc(valueInput = e.target.value);
+                }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={filterMovie} />
       </div>
 
       <div className="sidebar">
