84873863a532fb77e44925a7b960d264497af09d
Katarzyna Bogiel <katarzyna.bogiel@poczta.onet.pl>
2023-05-16 18:36:08 +0200

    initial version

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+const visibleMovies = (query: string) => {
+  return moviesFromServer.filter(movie => {
+    const title = movie.title.toLowerCase();
+    const description = movie.description.toLowerCase();
+    const trimmedQuery = query.toLowerCase().trim();
+
+    return title.includes(trimmedQuery) || description.includes(trimmedQuery);
+  });
+};
+
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +35,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={handleInputChange}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies(query)} />
       </div>
 
       <div className="sidebar">
