f824746afba5f70d49d5e6a476ff03f46c2aaffa
Kacper Łyczba <suaby1725@gmail.com>
2023-05-17 10:05:41 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [visibleMovies, setMovies] = useState(moviesFromServer);
+
+  const filterByQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value.trim(); // trim due to test 15, "should work if user types extra spaces before or after the query"
+
+    setMovies(moviesFromServer.filter((movie) => (
+      movie.title.toLowerCase().includes(query.toLowerCase())
+      || movie.description.toLowerCase().includes(query.toLowerCase())
+    )));
+  };
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +31,13 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={filterByQuery}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
