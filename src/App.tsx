4399ef1e8aa3d81071d43a52b1d70600c2105644
catgode <105614630+catgode@users.noreply.github.com>
2023-05-19 15:25:25 +0200

    change the input field into a controlled input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const handleQueryChange = (event: React.SetStateAction<string>) => {
+    setQuery(event);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
@@ -19,7 +25,9 @@ export const App: React.FC = () => {
                 type="text"
                 id="search-query"
                 className="input"
+                value={query}
                 placeholder="Type search word"
+                onChange={event => handleQueryChange(event.target.value)}
               />
             </div>
           </div>
