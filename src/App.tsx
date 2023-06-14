fefb44bb7ea4569582a63574c6e7972ba1586c66
Anna Peretiatko <tobossanna@gmail.com>
2023-06-14 15:05:54 +0200

    React movies filter solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const [visibleMovies, setFilteredMovies] = useState(moviesFromServer);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setQuery(value);
+
+    const filteredMovies = moviesFromServer.filter(
+      movie => movie.title
+        .toLowerCase()
+        .includes(value.toLowerCase().trim())
+      || movie.description
+        .toLowerCase()
+        .includes(value.toLowerCase().trim()),
+    );
+
+    setFilteredMovies(filteredMovies);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +40,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={handleChange}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
