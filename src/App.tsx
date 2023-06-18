2c95702fd05fb9752618c6e3a2bb96b792f064d8
Viktor Panchenko <panch.v1811@gmail.com>
2023-06-18 19:32:27 +0200

    react_movies-list-filter solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleFilms = (movies: Movie[], searchQuery: string): Movie[] => {
+    return movies.filter(movie => {
+      const normalizedTitle = movie.title.toLowerCase();
+      const normalizedDescription = movie.description.toLowerCase();
+      const normalizedQuery = searchQuery.toLowerCase();
+
+      return normalizedTitle.includes(normalizedQuery)
+        || normalizedDescription.includes(normalizedQuery);
+    });
+  };
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +33,15 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={(event) => {
+                  setQuery((event.target.value).toLowerCase().trim());
+                }}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleFilms(moviesFromServer, query)} />
       </div>
 
       <div className="sidebar">
