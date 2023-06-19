4f3ef1634b91a7f5afd9c8e52b4e575d7faed39e
Vladyslav Lapshyn <lapshin.vlad96@gmail.com>
2023-06-19 09:21:32 +0300

    made react task movies-list-filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import { FC, useEffect, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App: React.FC = () => {
+export const App: FC = () => {
+  const [visibleMovies, setVisibleMovies] = useState(moviesFromServer);
+  const [query, setQuery] = useState('');
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  useEffect(() => {
+    const filteredMovies = moviesFromServer.filter((movie) => {
+      const lowerCaseSearchQuery = query.toLowerCase().trim();
+
+      return [movie.title, movie.description].some(
+        (text) => text.toLowerCase().includes(lowerCaseSearchQuery),
+      );
+    });
+
+    setVisibleMovies(filteredMovies);
+  }, [query]);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +39,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={handleSearch}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
