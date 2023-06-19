47ce9e106ce799db0a19dd90db7914132afa2de3
Oleksii Lysenko <nano77@ukr.net>
2023-06-19 17:04:10 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState('');
-  const [visibleMovies, setVisibleMovies] = useState([...moviesFromServer]);
 
-  useEffect(() => {
-    setVisibleMovies(
-      searchQuery
-        ? moviesFromServer.filter(movie => {
-          const title = movie.title.toLowerCase();
-          const description = movie.description.toLowerCase();
-          const querry = searchQuery.trim().toLowerCase();
+  const visibleMovies = moviesFromServer.filter(movie => {
+    const title = movie.title.toLowerCase();
+    const description = movie.description.toLowerCase();
+    const querry = searchQuery.trim().toLowerCase();
 
-          return title.search(querry) >= 0 || description.search(querry) >= 0;
-        })
-        : [...moviesFromServer],
-    );
-  }, [searchQuery]);
+    return title.search(querry) >= 0 || description.search(querry) >= 0;
+  });
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(event.target.value);
