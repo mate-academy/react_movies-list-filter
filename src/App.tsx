95445ea5f917a4c03646763f393febd4d85f7155
holytimy4 <ez.lvl.faceit@gmail.com>
2023-06-20 01:48:50 +0300

    added task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const filteredMovies = moviesFromServer.filter(movie => {
+    const {
+      title,
+      description,
+    } = movie;
+
+    const searchRegex = new RegExp(query.trim(), 'gi');
+    const isMatch = searchRegex.test(title) || searchRegex.test(description);
+
+    return isMatch;
+  });
+
+  const searchInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setQuery(value);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -20,12 +39,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={searchInputHandler}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={filteredMovies} />
       </div>
 
       <div className="sidebar">
