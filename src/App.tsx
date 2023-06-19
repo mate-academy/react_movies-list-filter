4fd3f11705af761425c813927e688ee143ca8de8
Andrii Mashutikov <mashutikov.a.o@gmail.com>
2023-06-19 17:59:31 +0300

    Fixed comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+interface Event {
+  target: {
+    value: string;
+  }
+}
+
 export const App: React.FC = () => {
   const [query, setQuery] = useState('');
+  const [visibleMovies, setVisibleMovies] = useState(moviesFromServer);
 
-  const handleQueryChange = (event: { target: { value: string; }; }) => {
+  const handleQueryChange = (event: Event) => {
     const { value } = event.target;
 
     setQuery(value);
   };
 
-  const visibleMovies = moviesFromServer.filter(movie => {
-    const searchQuery = query.trim().toLowerCase();
+  useEffect(() => {
+    setVisibleMovies(moviesFromServer.filter(movie => {
+      const searchQuery = query.trim().toLowerCase();
 
-    const checkString = `
-      ${movie.title}
-      ${movie.description}
-    `;
+      const movieInfo = `
+        ${movie.title}
+        ${movie.description}
+      `;
 
-    return checkString.toLowerCase().includes(searchQuery);
-  });
+      return movieInfo.toLowerCase().includes(searchQuery);
+    }));
+  }, [query]);
 
   return (
     <div className="page">
