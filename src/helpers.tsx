49d924d8ce61fc0ae255181f750005e624f2922a
Ulia Losytska <ulositska@gmail.com>
2023-06-19 13:47:38 +0300

    improve readability

--- src/helpers.tsx
+++ src/helpers.tsx
@@ -1,10 +1,13 @@
-export const filterMovies = (movies:Movie[], searchQuery:string) => {
+export const filterMovies = (movies: Movie[], searchQuery: string) => {
   const searchQueryNormilized = searchQuery.toLowerCase().trim();
 
   return movies.filter(movie => {
+    const titleNormilized = movie.title.toLowerCase();
+    const descriptionNormilized = movie.description.toLowerCase();
+
     const stringToCheck = `
-    ${movie.title.toLowerCase()}
-    ${movie.description.toLowerCase()}
+    ${titleNormilized}
+    ${descriptionNormilized}
     `;
 
     return stringToCheck.includes(searchQueryNormilized);
