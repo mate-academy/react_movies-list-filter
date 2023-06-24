4db21ed66646fa3e0b6c98edaf6556682b22050f
Misha Grinko <glukym@gmail.com>
2023-06-24 18:12:52 +0300

    Update MovieCard.jsx

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,39 +1,31 @@
 import './MovieCard.scss';
 
-export const MovieCard = ({ movie }) => {
-  const { imdbUrl, imgUrl, description, title } = movie;
+export const MovieCard = ({ movie }) => (
+  <div className="card">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img src={movie.imgUrl} alt="Film logo" />
+      </figure>
+    </div>
 
-  return (
-    <div className="card">
-      <div className="card-image">
-        <figure className="image is-4by3">
-          <img
-            src={imgUrl}
-            alt="Film logo"
-          />
-        </figure>
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8">{movie.title}</p>
+        </div>
       </div>
-      <div className="card-content">
-        <div className="media">
-          <div className="media-left">
-            <figure className="image is-48x48">
-              <img
-                src="images/imdb-logo.jpeg"
-                alt="imdb"
-              />
-            </figure>
-          </div>
-          <div className="media-content">
-            <p className="title is-8">{title}</p>
-          </div>
-        </div>
 
-        <div className="content">
-          {description}
-          <br />
-          <a href={imdbUrl}>IMDB</a>
-        </div>
+      <div className="content">
+        {movie.description}
+        <br />
+        <a href={movie.imdbUrl}>IMDB</a>
       </div>
     </div>
-  );
-};
+  </div>
+);
