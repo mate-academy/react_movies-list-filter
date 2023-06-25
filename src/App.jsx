28fff2f07cc6008e7bd464ebde04b0979cf0643b
Elijah Vinkov <illia.vinkov@gmail.com>
2023-06-25 21:10:57 +0200

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+const App = () => {
+  const [query, setQuery] = useState('');
+  const meetsCondition
+  = string => string.toLowerCase().includes(query.trim().toLowerCase());
+  const visibleMovies = moviesFromServer.filter(
+    movie => meetsCondition(movie.title) || meetsCondition(movie.description),
+  );
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={query}
+                onChange={event => setQuery(event.target.value)}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
+  );
+};
 
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+export default App;
