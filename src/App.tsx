10b2ef55d868e3062e55d81200286ef8a9cc6cff
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2023-07-22 20:35:23 +0300

    react_movies-list-filter-js

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,45 @@
-import React from 'react';
+import { useState } from 'react';
+
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
+import { SearchBar } from './components/SearchBar';
 import moviesFromServer from './api/movies.json';
 
-export const App: React.FC = () => {
+import { Movie } from './types/Movie';
+
+function prepareVisibleMovies(
+  moviesList: Movie[],
+  query: string,
+): Movie[] {
+  const movies = [...moviesList].filter((movie) => {
+    const { title, description } = movie;
+
+    if (description.toLowerCase().includes(query.toLowerCase().trim())) {
+      return true;
+    }
+
+    if (title.toLowerCase().includes(query.toLowerCase().trim())) {
+      return true;
+    }
+
+    return false;
+  });
+
+  return movies;
+}
+
+export const App = () => {
+  const [query, setQuery] = useState<string>('');
+  const visibleMovies = prepareVisibleMovies(moviesFromServer, query);
+
   return (
     <div className="page">
       <div className="page-content">
-        <div className="box">
-          <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="search-query" className="label">
-              Search movie
-            </label>
-
-            <div className="control">
-              <input
-                type="text"
-                id="search-query"
-                className="input"
-                placeholder="Type search word"
-              />
-            </div>
-          </div>
-        </div>
-
-        <MoviesList movies={moviesFromServer} />
+        <SearchBar sortByQuery={(queryString) => {
+          setQuery(queryString);
+        }}
+        />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
