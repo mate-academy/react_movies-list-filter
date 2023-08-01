e944a371bd975ffaed6952e8607a6ab1ef401d87
Anna Akhalmosulishvili <akhala33@gmail.com>
2023-08-01 11:38:28 +0300

    solution

---
+++ src/components/SearchField/SearchField.tsx
@@ -0,0 +1,19 @@
+interface Props {
+  filterBy(value: string) :void;
+}
+
+export const SearchField: React.FC<Props> = ({ filterBy }) => {
+  return (
+    <div className="control">
+      <input
+        type="text"
+        id="search-query"
+        className="input"
+        placeholder="Type search word"
+        onChange={(e) => {
+          filterBy(e.target.value);
+        }}
+      />
+    </div>
+  );
+};
