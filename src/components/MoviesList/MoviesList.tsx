741e4d54bbe865240d8567994e104b9353cb3012
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-08-15 13:31:38 +0200

    add task solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,15 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 import React from 'react';
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
 interface Props {
   movies: Movie[];
+  query: string;
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList: React.FC<Props> = ({ movies, query }) => {
+  let visibleMovies = false;
+
+  return (
+    <div className="movies">
+      {movies.map(movie => {
+        movie.title.toLowerCase().includes(query.toLowerCase().trim())
+        || movie.description.toLowerCase().includes(query.toLowerCase().trim())
+          ? visibleMovies = true
+          : visibleMovies = false;
+
+        return (
+          (visibleMovies && <MovieCard key={movie.imdbId} movie={movie} />)
+        );
+      })}
+    </div>
+  );
+};
