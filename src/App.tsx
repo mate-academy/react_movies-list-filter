7bb9f282b31adea320c314bc8157e26d8bd11420
Emil Owczarek <owczarekemil@gmail.com>
2023-09-10 18:59:32 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setQuery(event.target.value);
+  }
+
+  const visibleMovies = moviesFromServer.filter(movie => {
+    const trimmedQuery = query.trim();
+    const lowerCaseQuery = trimmedQuery.toLowerCase();
+
+    return movie.title.toLowerCase().includes(lowerCaseQuery)
+      || movie.description.toLowerCase().includes(lowerCaseQuery);
+  });
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +34,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={handleChange}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
