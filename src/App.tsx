c33c6ed144603e05802060d8e13f7038686d33c2
kate-prakofyeva <catherine.smiff@gmail.com>
2023-12-06 18:48:39 +0100

    add task colution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState<string>('');
+
+  const visibleMovies = moviesFromServer.filter(movie => {
+    const searchWord = query.toLowerCase().trim();
+    const titleMatches = movie.title.toLowerCase()
+      .includes(searchWord);
+    const descriptionMatches = movie.description.toLowerCase()
+      .includes(searchWord);
+
+    return titleMatches || descriptionMatches;
+  });
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +32,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
