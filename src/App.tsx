3fd8dd603d413445d75a8967bfeb0909e404c3d6
Timi2424 <tymonbieganski@wp.pl>
2023-12-11 11:40:01 +0100

    apply  mvjl000 suggestions

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,44 @@
-import React, { FormEventHandler, useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+function movieFilter(movies: Movie[], query: string) {
+  return movies.filter(
+    movie => movie.title.toLowerCase().includes(query.trim().toLowerCase())
+    || movie.description.toLowerCase().includes(query.trim().toLowerCase()),
+  );
+}
+
 export const App: React.FC = () => {
   const [query, setQuery] = useState('');
 
-  const handleSubmit: FormEventHandler = (event) => {
-    event.preventDefault();
+  const handleMovies: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setQuery(event.target.value);
   };
 
-  const visibleMovies = moviesFromServer.filter(
-    item => item.title.toLowerCase().includes(query.toLowerCase())
-    || item.description.toLowerCase().includes(query.toLowerCase()),
-  );
+  const visibleMovies = movieFilter(moviesFromServer, query);
 
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            <form onSubmit={handleSubmit}>
-              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
 
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                  onChange={(event) => setQuery(event.target.value.trim())}
-                />
-              </div>
-            </form>
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={query}
+                onChange={handleMovies}
+              />
+            </div>
           </div>
         </div>
 
