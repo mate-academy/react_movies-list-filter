4d0457dd0e218265d23c265743f4dd04d3536214
Tommaso Giacopini <giacopini@bluewin.ch>
2023-12-20 22:02:04 +0100

    created helper function

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+const normalizeString = (str: any) => {
+  return str.toLowerCase().replace(/\s+/g, ' ').trim();
+};
+
 export const App: React.FC = () => {
   const [query, setQuery] = useState('');
 
   const visibleMovies = moviesFromServer.filter((movie) => {
-    const normalizedQuery = query.toLowerCase().trim();
-    const normalizedTitle
-    = movie.title.toLowerCase().replace(/\s+/g, ' ').trim();
-    const normalizedDescription
-    = movie.description.toLowerCase().replace(/\s+/g, ' ').trim();
+    const normalizedQuery = normalizeString(query);
+    const normalizedTitle = normalizeString(movie.title);
+    const normalizedDescription = normalizeString(movie.description);
 
     return (
-      normalizedTitle.includes(normalizedQuery)
-      || normalizedDescription.includes(normalizedQuery)
+      normalizedTitle.includes(normalizedQuery) ||
+      normalizedDescription.includes(normalizedQuery)
     );
   });
 
