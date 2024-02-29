cf7752e8f996ce4f2a7326ae1228a4afadf8004b
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-02-29 18:20:59 +0100

    change trim() to trimStart()

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export const App: React.FC = () => {
   const handleSearchChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = ev.target.value;
 
-    setSearch(newValue.trim());
+    setSearch(newValue.trimStart());
   };
 
   return (
