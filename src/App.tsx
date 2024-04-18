684a01eda96ff1ae05d8d5ea474ab3a82ed18be1
Allanpt <jatallantavares@gmail.com>
2024-04-18 18:51:57 -0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  interface Move {
+    title: string;
+    description: string;
+    imgUrl: string;
+    imdbUrl: string;
+    imdbId: string;
+  }
+
+  function handleQuery(newQuery: string) {
+    setQuery(newQuery);
+  }
+
+  function checkIncludesQuery(movie: Move) {
+    const finalQuery = query.toLowerCase().trim();
+
+    return (
+      movie.title.toLowerCase().includes(finalQuery) ||
+      movie.description.toLowerCase().includes(finalQuery)
+    );
+  }
+
+  const visibleMovies: Move[] = moviesFromServer.filter(checkIncludesQuery);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -16,16 +41,18 @@ export const App: React.FC = () => {
 
             <div className="control">
               <input
+                value={query}
                 type="text"
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={e => handleQuery(e.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">Sidebar goes here</div>
