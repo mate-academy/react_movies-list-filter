3e566aef519be91aeca9bd1234263bdb9ec06ffe
Rafael Wesley <rafaelwesleylima@gmail.com>
2024-04-19 16:06:47 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  const filterListMovies = (searchString: string) => {
+    setQuery(searchString.toLocaleLowerCase().trim());
+  };
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +26,18 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={event => filterListMovies(event.currentTarget.value)}
               />
             </div>
           </div>
         </div>
-
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList
+          movies={moviesFromServer.filter(
+            e =>
+              e.title.toLowerCase().includes(query) ||
+              e.description.toLowerCase().includes(query),
+          )}
+        />
       </div>
 
       <div className="sidebar">Sidebar goes here</div>
