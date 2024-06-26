25e7878f5369f46b70ec1f770f79e1667574d522
VitaliiPa <poroh1991ukr@gmail.com>
2024-06-26 22:47:12 +0300

    added search

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
+import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
-import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(<App />, document.getElementById('root'));
