19268be12ad99219ce37842ed6157e8374ecd4c7
Jakub Bajkowski <k.bajkowski7@wp.pl>
2024-12-16 04:37:21 +0100

    add task solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-interface Props {
-  movies: Movie[];
+export interface Movie {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
 }
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+interface Props {
+  movies: Movie[];
+  movieTitle: string;
+}
+
+export const MoviesList: React.FC<Props> = ({ movies, movieTitle }) => {
+  const [moviesToShow, setMoviesToShow] = useState(movies);
+
+  useEffect(() => {
+    const visibleMovies = movies.filter(
+      movie =>
+        movie.title.toLowerCase().includes(movieTitle.toLowerCase().trim()) ||
+        movie.description
+          .toLowerCase()
+          .includes(movieTitle.toLowerCase().trim()),
+    );
+
+    setMoviesToShow(visibleMovies);
+  }, [movieTitle, movies]);
+
+  return (
+    <div className="movies">
+      {moviesToShow.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
